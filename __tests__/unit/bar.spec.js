d1429699c55e373f1b1a4712b37b4f1846a3331e
weepy3641 <min.hu.sc@gmail.com>
2019-05-30 09:26:34 +0800

    feat(g2-plot): add basic bar unit test

--- __tests__/unit/bar.spec.js
+++ __tests__/unit/bar.spec.js
@@ -1,4 +1,5 @@
 import { Bar } from '../../src';
+import { expect } from 'chai';
 
 describe('Bar plot', () => {
   const canvasDiv = document.createElement('div');
@@ -9,67 +10,78 @@ describe('Bar plot', () => {
   canvasDiv.id = 'canvas1';
   document.body.appendChild(canvasDiv);
 
-  it('基础柱状图', () => {
-    const data = [ {
-      year: '1991',
-      value: 3
-    }, {
-      year: '1992',
-      value: 4
-    }, {
-      year: '1993',
-      value: 3.5
-    }, {
-      year: '1994',
-      value: 5
-    }, {
-      year: '1995',
-      value: 4.9
-    }, {
-      year: '1996',
-      value: 0.5
-    }, {
-      year: '1997',
-      value: 7
-    }, {
-      year: '1998',
-      value: 9
-    }, {
-      year: '1999',
-      value: 13
-    } ];
+  const data = [ {
+    year: '1991',
+    value: 3
+  }, {
+    year: '1992',
+    value: 4
+  }, {
+    year: '1993',
+    value: 3.5
+  }, {
+    year: '1994',
+    value: 5
+  }, {
+    year: '1995',
+    value: 4.9
+  }, {
+    year: '1996',
+    value: 0.5
+  }, {
+    year: '1997',
+    value: 7
+  }, {
+    year: '1998',
+    value: 9
+  }, {
+    year: '1999',
+    value: 13
+  } ];
 
-    const columnPlot = new Bar(canvasDiv, {
-      padding: [ 10, 80, 80, 80 ],
+  it('基础条形图', () => {
+    const barPlot = new Bar(canvasDiv, {
+      padding: 'auto',
       data,
       xField: 'value',
       yField: 'year',
-      // barSize: 10,
-      xAxis: {
-        min: 0,
-        // visible: false
-      },
-      yAxis: {
-        // visible: false
-      },
-      // tooltip: false,
-      label: {
-        // visible: false,
-        offset: 0,
-        adjustColor: true,
-        position: 'middle',
-        /* formatter: (val) => {
-          return val + ' label';
-        },
-        offsetX: 10,
-        offsetY: 20,
-        style: {
-          fill: 'red'
-        }*/
-      }
     });
-
-    columnPlot.render();
+    barPlot.render();
+    const positionField = barPlot.plot.get('elements')[0].get('position').fields;
+    const isTransposed = barPlot.plot.get('coord').isTransposed;
+    expect(barPlot).to.be.instanceOf(Bar);
+    expect(positionField[0]).to.be.equal('year');
+    expect(positionField[1]).to.be.equal('value');
+    expect(isTransposed).to.be.equal(true);
+    barPlot.destroy();
+    expect(barPlot.plot.destroyed).to.be.true;
   });
+
+  // it('基础条形图 xAxis false', () => {
+  //   const barPlot = new Bar(canvasDiv, {
+  //     padding: 'auto',
+  //     data,
+  //     xField: 'value',
+  //     yField: 'year',
+  //     xAxis: {
+  //       visible: true,
+  //       style: {
+  //         line: { stroke: 'red' },
+  //       }
+  //     }
+  //   });
+  //   barPlot.render();
+  //   const axis = barPlot.plot.get('axisController').axes[0];
+  //   console.log(axis);
+
+  //   // const positionField = barPlot.plot.get('elements')[0].get('position').fields;
+  //   // const isTransposed = barPlot.plot.get('coord').isTransposed;
+  //   // expect(barPlot).to.be.instanceOf(Bar);
+  //   // expect(positionField[0]).to.be.equal('year');
+  //   // expect(positionField[1]).to.be.equal('value');
+  //   // expect(isTransposed).to.be.equal(true);
+  //   // barPlot.destroy();
+  //   // expect(barPlot.plot.destroyed).to.be.true;
+  // });
 
 });

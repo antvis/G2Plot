ad71f991f011e206c1fc6a92074fe5645df923f4
hm.hm <hm.hm@alibaba-inc.com>
2019-08-29 15:45:19 +0800

    fix(theme) remove no need theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,8 +3,6 @@ import defaultTheme from './default';
 
 import aliDark from './ali-dark';
 import aliLight from './ali-light';
-import antDark from './ant-dark';
-import antLight from './ant-light';
 import Theme from './theme';
 
-export { Theme as default, defaultTheme, aliLight, aliDark, antLight, antDark };
+export { Theme as default, defaultTheme, aliLight, aliDark};

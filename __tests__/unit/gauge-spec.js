7c4926b1b14b2a44ae927a8a1965289d9ec9ad4e
ssfxz <lyssfxz@126.com>
2019-09-11 16:04:20 +0800

    feat(plot): update liquid & gauge unit test.

--- __tests__/unit/gauge-spec.js
+++ __tests__/unit/gauge-spec.js
@@ -1,3 +1,4 @@
+import { expect } from 'chai';
 import { Gauge } from '../../src';
 import Theme from '../../src/theme/theme';
 
@@ -27,11 +28,40 @@ describe('Gauge plot', () => {
       max: 100,
       range: [20, 40, 60, 80],
       label: true,
+      format: (d) => `[${d}]`,
+    });
+    gaugePlot.render();
+    expect(gaugePlot).to.be.instanceOf(Gauge);
+    const positionField = gaugePlot.plot.get('elements')[0].get('position').fields;
+    expect(positionField[0]).to.be.equal('value');
+    expect(positionField[1]).to.be.equal('1');
+    expect(gaugePlot.plot.get('coord').type).to.be.equal('polar');
+    gaugePlot.destroy();
+    expect(gaugePlot.plot.destroyed).to.be.true;
+  });
+
+
+  it.only('gauge style', () => {
+    // Theme.setTheme('ali-light');
+    const gaugePlot = new Gauge(canvasDiv, {
+      title: {
+        text: '水位图测试',
+      },
+      description: {
+        text: '水位图用来展示数据在总集合中的占比情况，通过水位的样式展示信息。',
+      },
+      width: 600,
+      height: 650,
+
+      value: 64,
+      min: 0,
+      max: 100,
+      range: [20, 40, 60, 80],
       // label: (value, formatted) => `<div>${value}+++${formatted}</div>`,
-      // label: (value, formatted) => {const a = document.createElement("div"); a.innerText = `${formatted}`; return a;},
+      label: (value, formatted) => {const a = document.createElement("div"); a.innerText = `${formatted}`; return a;},
       format: (d) => `[${d}]`,
       gaugeStyle: {
-        // colors: [ '#1890FF', '#2FC25B', '#FACC14', '#223273', '#8543E0', '#13C2C2', '#3436C7', '#F04864' ],
+        colors: [ '#1890FF', '#2FC25B', '#FACC14', '#223273', '#8543E0', '#13C2C2', '#3436C7', '#F04864' ],
         // stripWidth: 30,
         // stripBackColor: '#ddd',
         // tickInterval: 20,
@@ -40,12 +70,16 @@ describe('Gauge plot', () => {
         // tickLabelColor: '#aaa',
         // tickLineColor: '#aaa',
         // subTickCount: 4,
-        // pointerColor: '#1890FF',
+        pointerColor: '#1890FF',
         // labelPos: [ '80%', '50%' ],
         // labelColor: '#666',
         // labelSize: 30,
       },
     });
     gaugePlot.render();
+    const colorCfg = gaugePlot.plot.get('elements')[0].get('color').values;
+    expect(colorCfg[0]).to.be.equal('#1890FF');
+    gaugePlot.destroy();
+    expect(gaugePlot.plot.destroyed).to.be.true;
   });
 });
--- __tests__/unit/liquid-spec.js
+++ __tests__/unit/liquid-spec.js
@@ -1,3 +1,4 @@
+import { expect } from 'chai';
 import { Liquid } from '../../src';
 import Theme from '../../src/theme/theme';
 
@@ -27,16 +28,51 @@ describe('Liquid plot', () => {
       max: 10000,
       value: 6640,
       showValue: true,
+    });
+    liquidPlot.render();
+    expect(liquidPlot).to.be.instanceOf(Liquid);
+    const positionField = liquidPlot.plot.get('elements')[0].get('position').fields;
+    expect(positionField[0]).to.be.equal('1');
+    expect(positionField[1]).to.be.equal('value');
+    expect(liquidPlot.plot.get('coord').type).to.be.equal('cartesian');
+    liquidPlot.destroy();
+    expect(liquidPlot.plot.destroyed).to.be.true;
+  });
+
+
+  it.only('liquid-style', () => {
+    // Theme.setTheme('ali-light');
+    const liquidPlot = new Liquid(canvasDiv, {
+      title: {
+        text: '水位图测试',
+      },
+      description: {
+        text: '水位图用来展示数据在总集合中的占比情况，通过水位的样式展示信息。',
+      },
+      width: 400,
+      height: 450,
+
+      type: 'normal',
+      min: 0,
+      max: 10000,
+      value: 6640,
+      showValue: true,
       format: (d) => `[${d}]`,
       liquidStyle: {
-        // borderOpacity: 0.2,
-        // borderWidth: 10,
-        // color: '#3B76FF',
-        // fontColor: '#233F7E',
-        // fontOpacity: 1,
-        // fontSize: 40
+        borderOpacity: 0.2,
+        borderWidth: 10,
+        color: '#3B76FF',
+        fontColor: '#233F7E',
+        fontOpacity: 1,
+        fontSize: 40
       },
     });
     liquidPlot.render();
+    const colorCfg = liquidPlot.plot.get('elements')[0].get('color').values;
+    expect(colorCfg[0]).to.be.equal('#3B76FF');
+    const annotationText = liquidPlot.plot.cfg.annotationController.annotations[0].cfg.content;
+    expect(annotationText).to.be.equal('[6640]');
+    liquidPlot.destroy();
+    expect(liquidPlot.plot.destroyed).to.be.true;
   });
 });

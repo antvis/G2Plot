32fb3e769752d1e1d7f44ba9909620724a664e6c
芈乐 <hm.hm@alibaba-inc.com>
2019-09-12 10:15:12 +0800

    feat: improve the stackarea plot test cases

--- __tests__/unit/stackArea-spec.js
+++ __tests__/unit/stackArea-spec.js
@@ -1,5 +1,5 @@
 import { StackArea } from '../../src';
-// import { expect } from 'chai';
+import { expect } from 'chai';
 
 describe('stack-area plot', () => {
   const canvasDiv = document.createElement('div');
@@ -105,6 +105,7 @@ describe('stack-area plot', () => {
 
   it('初始化以及销毁', () => {
     const areaPlot = new StackArea(canvasDiv, {
+      responsive: true,
       width: 600,
       height: 600,
       padding: 'auto',
@@ -120,37 +121,248 @@ describe('stack-area plot', () => {
       },
       point: {
         visible: false,
-        /*style: {
-          color: 'green',
-          size: 8,
-          lineWith: 3,
-        },*/
       },
       line: {
         visible: true,
-        /*style: {
-          color: 'blue',
-          size: 4,
-        },*/
       },
       label: {
         visible: true,
         type: 'area',
         autoScale: true
       },
-      // color: [ 'red', 'blue' ],
     });
     areaPlot.render();
-    // const positionField = areaPlot.plot.get('elements')[0].get('position').fields;
-    // const isTransposed = areaPlot.plot.get('coord').isTransposed;
-    // const axes = areaPlot.plot.get('axisController').axes;
+    const positionField = areaPlot.plot.get('elements')[0].get('position').fields;
+    const colorField = areaPlot.plot.get('elements')[0].get('color').fields;
+    const axes = areaPlot.plot.get('axisController').axes;
 
-    // expect(areaPlot).to.be.instanceOf(Area);
-    // expect(positionField[0]).to.be.equal('value');
-    // expect(positionField[1]).to.be.equal('year');
-    // expect(isTransposed).to.be.equal(false);
-    // expect(axes.length).to.be.equal(2);
-    // areaPlot.destroy();
-    // expect(areaPlot.plot.destroyed).to.be.true;
+    expect(areaPlot).to.be.instanceOf(StackArea);
+    expect(positionField[0]).to.be.equal('year');
+    expect(positionField[1]).to.be.equal('value');
+    expect(colorField[0]).to.be.equal('type');
+    expect(axes.length).to.be.equal(2);
+    areaPlot.destroy();
+    expect(areaPlot.plot.destroyed).to.be.true;
+  });
+
+  it('area shape attr map', () => {
+    const areaPlot = new StackArea(canvasDiv, {
+      responsive: true,
+      width: 600,
+      height: 600,
+      padding: 'auto',
+      data,
+      xField: 'year',
+      yField: 'value',
+      stackField: 'type',
+      color: ['red', 'pink'],
+      xAxis: {
+        visible: true,
+      },
+      yAxis: {
+        visible: true,
+      },
+      point: {
+        visible: false,
+      },
+      line: {
+        visible: true,
+      },
+      label: {
+        visible: true,
+        type: 'area',
+        autoScale: true
+      },
+    });
+    areaPlot.render();
+    const intervalEle = areaPlot.plot.get('elements')[0];
+    expect(intervalEle.get('color').values[0]).to.be.equal('red');
+    expect(intervalEle.get('color').values[1]).to.be.equal('pink');
+    areaPlot.destroy();
+    expect(areaPlot.plot.destroyed).to.be.true;
+  });
+
+  it('point shape attr map and style', () => {
+    const areaPlot = new StackArea(canvasDiv, {
+      responsive: true,
+      width: 600,
+      height: 600,
+      padding: 'auto',
+      data,
+      xField: 'year',
+      yField: 'value',
+      stackField: 'type',
+      color: ['red', 'pink'],
+      xAxis: {
+        visible: true,
+      },
+      yAxis: {
+        visible: true,
+      },
+      point: {
+        visible: true,
+        size: 6,
+        color: 'yellow',
+        style: {
+          strokeStyle: 'black',
+          lineWidth: 2,
+        }
+      },
+      line: {
+        visible: false,
+      },
+      label: {
+        visible: true,
+        type: 'area',
+        autoScale: true
+      },
+    });
+    areaPlot.render();
+    const pointEle = areaPlot.plot.get('elements')[1];
+    expect(pointEle.get('type')).to.be.equal('point');
+    expect(pointEle.get('size').values[0]).to.be.equal(6);
+    expect(pointEle.get('color').values[0]).to.be.equal('yellow');
+    expect(pointEle.get('style').cfg.strokeStyle).to.be.equal('black');
+    expect(pointEle.get('style').cfg.lineWidth).to.be.equal(2);
+    areaPlot.destroy();
+    expect(areaPlot.plot.destroyed).to.be.true;
+  });
+
+  it('point style func', () => {
+    const styleFunc = d => {
+      return {
+        strokeStyle: 'black',
+      }
+    };
+    const areaPlot = new StackArea(canvasDiv, {
+      responsive: true,
+      width: 600,
+      height: 600,
+      padding: 'auto',
+      data,
+      xField: 'year',
+      yField: 'value',
+      stackField: 'type',
+      color: ['red', 'pink'],
+      xAxis: {
+        visible: true,
+      },
+      yAxis: {
+        visible: true,
+      },
+      point: {
+        visible: true,
+        size: 6,
+        color: 'yellow',
+        style: styleFunc
+      },
+      line: {
+        visible: false,
+      },
+      label: {
+        visible: true,
+        type: 'area',
+        autoScale: true
+      },
+    });
+    areaPlot.render();
+    const pointEle = areaPlot.plot.get('elements')[1];
+    expect(pointEle.get('style').callback).to.be.equal(styleFunc);
+    areaPlot.destroy();
+    expect(areaPlot.plot.destroyed).to.be.true;
+  });
+
+  it('line shape attr map and style', () => {
+    const areaPlot = new StackArea(canvasDiv, {
+      responsive: true,
+      width: 600,
+      height: 600,
+      padding: 'auto',
+      data,
+      xField: 'year',
+      yField: 'value',
+      stackField: 'type',
+      color: ['red', 'pink'],
+      xAxis: {
+        visible: true,
+      },
+      yAxis: {
+        visible: true,
+      },
+      point: {
+        visible: false,
+        size: 6,
+        color: 'yellow',
+      },
+      line: {
+        visible: true,
+        size: 4,
+        color: 'purple',
+        style: {
+          strokeStyle: 'blue',
+          lineWidth: 3,
+        }
+      },
+      label: {
+        visible: true,
+        type: 'area',
+        autoScale: true
+      },
+    });
+    areaPlot.render();
+    const lineEle = areaPlot.plot.get('elements')[1];
+    expect(lineEle.get('type')).to.be.equal('line');
+    expect(lineEle.get('size').values[0]).to.be.equal(4);
+    expect(lineEle.get('color').values[0]).to.be.equal('purple');
+    expect(lineEle.get('style').cfg.strokeStyle).to.be.equal('blue');
+    expect(lineEle.get('style').cfg.lineWidth).to.be.equal(3);
+    areaPlot.destroy();
+    expect(areaPlot.plot.destroyed).to.be.true;
+  });
+
+  it('line style func', () => {
+    const styleFunc = d => {
+      return {
+        strokeStyle: 'black',
+      }
+    };
+    const areaPlot = new StackArea(canvasDiv, {
+      responsive: true,
+      width: 600,
+      height: 600,
+      padding: 'auto',
+      data,
+      xField: 'year',
+      yField: 'value',
+      stackField: 'type',
+      color: ['red', 'pink'],
+      xAxis: {
+        visible: true,
+      },
+      yAxis: {
+        visible: true,
+      },
+      point: {
+        visible: false,
+        size: 6,
+        color: 'yellow',
+      },
+      line: {
+        visible: true,
+        size: 4,
+        color: 'purple',
+        style: styleFunc,
+      },
+      label: {
+        visible: true,
+        type: 'area',
+        autoScale: true
+      },
+    });
+    areaPlot.render();
+    const lineEle = areaPlot.plot.get('elements')[1];
+    expect(lineEle.get('style').callback).to.be.equal(styleFunc);
+    areaPlot.destroy();
+    expect(areaPlot.plot.destroyed).to.be.true;
   });
 });

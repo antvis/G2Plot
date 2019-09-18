d65d25f30bbec33ee7ed1b95cb368e5e77010a89
paleface001 <mipha.ly@antfin.com>
2019-09-18 20:00:54 +0800

    chore(responsive): applyResponsive模块抽象

--- src/plots/column/applyResponsive/label.ts
+++ src/plots/column/applyResponsive/label.ts
@@ -1,38 +1,23 @@
 import * as _ from '@antv/util';
-import Responsive from '../../../util/responsive/responsive';
-import ShapeNodes from '../../../util/responsive/shapeNodes';
+import ApplyResponsiveLabel from '../../../util/responsive/apply/label';
 
-export default function responsiveLabel(plot) {
-  const responsiveTheme = plot.themeController.responsiveTheme;
-  if(!responsiveTheme.label){
-    return;
-  }
-  const labelResponsiveType = getLabelResponsiveType(plot);
+class ApplyResponsiveColumnLabel extends ApplyResponsiveLabel {
 
-  if (responsiveTheme.label[labelResponsiveType]) {
-    const element = plot.plot.get('elements')[0];
-    const labelShapes = element.get('labels');
-    const nodes = new ShapeNodes({
-      shapes: labelShapes,
-    });
-    const region = plot.plot.get('panelRange');
-    const { constraints, rules } = responsiveTheme.label[labelResponsiveType];
-    new Responsive({
-      nodes,
-      constraints,
-      rules,
-      region,
-      plot,
-    });
+  protected getType(){
+    if (this.plot.column.label) {
+      if (!this.plot.column.label.position || this.plot.column.label.position === 'top') {
+        return 'top';
+      }
+    }
+    return 'inner';
   }
+  
 }
 
-function getLabelResponsiveType(plot){
-  if (plot.column.label) {
-    if (!plot.column.label.position || plot.column.label.position === 'top') {
-      return 'top';
-    }
-  }
-
-  return 'inner';
+export default function responsivePointLabel(plot) {
+  const responsiveTheme = plot.themeController.responsiveTheme;
+  const applyResponsiveColumnLabel = new ApplyResponsiveColumnLabel({
+    plot,
+    responsiveTheme
+  });  
 }

543457cc0f408bf7c219027b2b5c4e84c8c2e4c2
zqlu <zqlu.cn@gmail.com>
2019-10-10 16:38:06 +0800

    fix: replace lodash usage with antv/util

--- src/plots/stack-area/component/label/lineLabel.ts
+++ src/plots/stack-area/component/label/lineLabel.ts
@@ -1,15 +1,15 @@
 import { Shape } from '@antv/g';
 import { ElementLabels, registerElementLabels } from '@antv/g2';
-import _ from 'lodash';
+import { deepMix, each } from '@antv/util';
 
 const MARGIN = 10;
 
 class AreaLineElementLabels extends ElementLabels {
   public showLabels(points: any, shapes: Shape[]) {
     const labelPoints = [];
-    _.each(shapes, (shape) => {
+    each(shapes, (shape) => {
       const originData = shape.get('origin');
-      const lastPoint = _.cloneDeep(originData[originData.length - 1]);
+      const lastPoint = deepMix({}, originData[originData.length - 1]);
       const bbox = shape.getBBox();
       lastPoint.x = bbox.maxX + MARGIN;
       lastPoint.y = lastPoint.y[1];
@@ -19,7 +19,7 @@ class AreaLineElementLabels extends ElementLabels {
     const renderer = this.get('labelsRenderer');
     const labels = renderer.get('group').get('children');
     const view = this.get('element').get('view');
-    _.each(labels, (label) => {
+    each(labels, (label) => {
       label.attr('textAlign', 'left');
       label.attr('textBaseline', 'middle');
       const origin = label.get('origin');
@@ -32,7 +32,7 @@ class AreaLineElementLabels extends ElementLabels {
 
   public _adjustColor(shapeId, shapes) {
     let color;
-    _.each(shapes, (shape) => {
+    each(shapes, (shape) => {
       const id = shape.id;
       if (id === shapeId) {
         color = shape.attr('fill');

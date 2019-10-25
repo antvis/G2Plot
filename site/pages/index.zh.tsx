03808da60ac2f2fc665780ce26f5392746c61874
afc163 <afc163@gmail.com>
2019-10-25 17:15:14 +0800

    :up: upgrade @antv/gatsby-theme-antv to 0.5.0

--- site/pages/index.zh.tsx
+++ site/pages/index.zh.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Skeleton } from 'antd';
 import { useTranslation } from 'react-i18next';
-import SEO from '@antv/gatsby-theme-antv/site/components/seo';
+import SEO from '@antv/gatsby-theme-antv/site/components/Seo';
 import astronaut from '../images/gatsby-astronaut.png';
 
 const IndexPage = () => {
@@ -9,7 +9,7 @@ const IndexPage = () => {
   return (
     <>
       <SEO title="蚂蚁数据可视化" lang="zh" />
-      <div style={{ margin: '0 auto', padding: '0 80px' }}>
+      <div style={{ margin: '0 auto', padding: '0 60px' }}>
         {t('首页')}
         <img src={astronaut} alt="astronaut" height="200" />
         <Skeleton />

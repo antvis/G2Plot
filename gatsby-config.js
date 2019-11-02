b2a011729a122e7184cb92ea1fff926bbaccf14f
afc163 <afc163@gmail.com>
2019-11-02 17:44:12 +0800

    docs(config): update gatsby-config.js for new theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,31 +15,14 @@ module.exports = {
     title: 'G2Plot',
     description: 'A collection of charts made with the Grammar of Graphics',
     githubUrl: repository.url,
-    docs: [
+    navs: [
       {
-        slug: 'manual',
+        slug: 'docs/manual',
         title: {
           zh: '使用文档',
           en: 'docs',
         },
         redirect: 'introduction',
-        order: 0,
-      },
-      {
-        slug: 'manual/plots',
-        title: {
-          zh: '图表',
-          en: 'Charts',
-        },
-        order: 3,
-      },
-      {
-        slug: 'manual/advanced',
-        title: {
-          zh: '进阶',
-          en: 'Advanced',
-        },
-        order: 4,
       },
       {
         slug: 'examples',
@@ -47,13 +30,28 @@ module.exports = {
           zh: '图表演示',
           en: 'Examples',
         },
-        order: 100,
         redirect: 'line/basic',
+      },
+    ],
+    docs: [
+      {
+        slug: 'manual/plots',
+        title: {
+          zh: '图表',
+          en: 'Charts',
+        },
+      },
+      {
+        slug: 'manual/advanced',
+        title: {
+          zh: '进阶',
+          en: 'Advanced',
+        },
       },
     ],
     examples: [
       {
-        slug: 'examples/line',
+        slug: 'line',
         icon: 'line', // 图标名可以去 https://antv.alipay.com/zh-cn/g2/3.x/demo/index.html 打开控制台查看图标类名
         title: {
           zh: '折线图',
@@ -61,8 +59,8 @@ module.exports = {
         },
       },
       {
-        slug: 'examples/pie',
-        icon: 'pie', // 图标名可以去 https://antv.alipay.com/zh-cn/g2/3.x/demo/index.html 打开控制台查看图标类名
+        slug: 'pie',
+        icon: 'pie',
         title: {
           zh: '饼图',
           en: 'Pie Charts',

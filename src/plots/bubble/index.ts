af9b48b35d53334406b0eb8d2651234f95d7fc9a
paleface001 <mipha.ly@antfin.com>
2019-11-07 23:00:53 +0800

    migrate bubble layer

--- src/plots/bubble/index.ts
+++ src/plots/bubble/index.ts
@@ -1,16 +1,15 @@
-import BasePlot from '../../base/plot';
+import * as _ from '@antv/util';
+import BasePlot, { PlotCfg } from '../../base/plot';
 import BubbleLayer, { BubbleLayerConfig } from './layer';
 
-export interface BubbleConfig extends BubbleLayerConfig {}
+export interface BubbleConfig extends BubbleLayerConfig, PlotCfg {}
 
-export default class Bubble<T extends BubbleLayerConfig = BubbleLayerConfig> extends BasePlot<T> {
-  protected init() {
-    const layer = new BubbleLayer(
-      this.getCanvasController(),
-      this.getThemeController(),
-      this.getPlotRange(),
-      this.initialProps
-    );
-    this.addLayer(layer);
+export default class Bubble<T extends BubbleConfig = BubbleConfig> extends BasePlot<T> {
+  public static getDefaultOptions: typeof BubbleLayer.getDefaultOptions = BubbleLayer.getDefaultOptions;
+
+  public createLayers(props) {
+    const layerProps = _.deepMix({}, props);
+    layerProps.type = 'bubble';
+    super.createLayers(layerProps);
   }
 }

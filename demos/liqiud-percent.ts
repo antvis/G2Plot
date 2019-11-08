2bb633a9de9f3ee950c9b86845d39b1f1caaab0f
jiefeng.yjf <jiefeng.yjf@koubei.com>
2019-11-08 22:34:26 +0800

    feat: demos都改成ts版本,方便验证types

---
+++ demos/liqiud-percent.ts
@@ -0,0 +1,13 @@
+// 百分比水位图
+
+const liquidPlot = new g2plot.Liquid(document.getElementById('canvas'), {
+  type: 'percent',
+  min: 0,
+  max: 10000,
+  value: 6640,
+  showValue: true,
+});
+liquidPlot.render();
+
+// 作为模块 避免变量冲突
+export {}
---
+++ demos/tinyPlot-progress-setColor.ts
@@ -0,0 +1,13 @@
+// 迷你图表-进度条:配置进度条颜色
+/* tslint:disable */
+const progress = new g2plot.Progress(document.getElementById('canvas'), {
+  width: 200,
+  height: 100,
+  percent: 0.3,
+  // color:'#2B46DB', //颜色设置为单值时配置当前进度的颜色
+  color: ['#E12B21', '#60ADA5'], // 颜色设置为数组时同时配置当前进度和背景的颜色
+});
+progress.render();
+
+// 作为模块 避免变量冲突
+export {}

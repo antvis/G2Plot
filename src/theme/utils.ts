feda0d5758f3f86dd8bc23a7a7da863fa7a28a3f
jiefeng.yjf <jiefeng.yjf@koubei.com>
2019-11-14 12:57:37 +0800

    make lint happy

--- src/theme/utils.ts
+++ src/theme/utils.ts
@@ -4,35 +4,35 @@ import * as _ from '@antv/util';
  * mutable 的方式修改 axis 配置
  * @param axis
  */
-function convertToG2Axis(axis: any): void {
-  if (axis.line && axis.line.style) {
-    const lineStyle = axis.line.style;
-    delete axis.line.style;
-    _.mix(axis.line, lineStyle);
-  }
-  if (axis.tickLine) {
-    const tickLineStyle = axis.tickLine.style;
-    delete axis.tickLine.style;
-    _.mix(axis.tickLine, tickLineStyle);
-  }
-  if (axis.grid) {
-    const gridStyle = axis.grid.style;
-    delete axis.grid.style;
-    _.mix(axis.grid, gridStyle);
-  }
-  if (axis.label) {
-    if (axis.label.style) {
-      axis.label.textStyle = axis.label.style;
-      delete axis.label.style;
-    }
-  }
-  if (axis.title) {
-    if (axis.title.style) {
-      axis.title.textStyle = axis.title.style;
-      delete axis.title.style;
-    }
-  }
-}
+// function convertToG2Axis(axis: any): void {
+//   if (axis.line && axis.line.style) {
+//     const lineStyle = axis.line.style;
+//     delete axis.line.style;
+//     _.mix(axis.line, lineStyle);
+//   }
+//   if (axis.tickLine) {
+//     const tickLineStyle = axis.tickLine.style;
+//     delete axis.tickLine.style;
+//     _.mix(axis.tickLine, tickLineStyle);
+//   }
+//   if (axis.grid) {
+//     const gridStyle = axis.grid.style;
+//     delete axis.grid.style;
+//     _.mix(axis.grid, gridStyle);
+//   }
+//   if (axis.label) {
+//     if (axis.label.style) {
+//       axis.label.textStyle = axis.label.style;
+//       delete axis.label.style;
+//     }
+//   }
+//   if (axis.title) {
+//     if (axis.title.style) {
+//       axis.title.textStyle = axis.title.style;
+//       delete axis.title.style;
+//     }
+//   }
+// }
 
 /**
  * 将图形主题转换成 g2 theme 格式

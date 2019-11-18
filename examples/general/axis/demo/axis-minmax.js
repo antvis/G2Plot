56681ad9b2ab1978909aea26f9e2c7e21a0aace4
paleface001 <mipha.ly@antfin.com>
2019-11-18 14:38:36 +0800

    wtf

--- examples/general/axis/demo/axis-minmax.js
+++ examples/general/axis/demo/axis-minmax.js
@@ -17,7 +17,6 @@ fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/fertility.json')
       xField: 'year',
       yField: 'value',
       seriesField: 'country',
-      smooth: true,
       xAxis: {
         visible: true,
         type: 'dateTime',

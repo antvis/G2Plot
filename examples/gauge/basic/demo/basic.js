5f212f92b3b06c58be2b1c8076726ce2481183de
paleface001 <mipha.ly@antfin.com>
2019-11-19 11:37:52 +0800

    千辛万苦更新一波demo~~~

--- examples/gauge/basic/demo/basic.js
+++ examples/gauge/basic/demo/basic.js
@@ -1,4 +1,27 @@
 import { Gauge } from '@antv/g2plot';
+import insertCss from 'insert-css';
+
+insertCss(`
+.g2plot-gauge-label {
+  width: 100px;
+  height: 100px;
+  vertical-align: middle;
+  text-align: center;
+  line-height: 0.2;
+}
+
+.g2plot-gauge-label .value {
+  font-size: 20px;
+  color: #8c8c8c;
+  font-weight: 300;
+}
+
+.g2plot-gauge-label .title {
+  font-size: 30px;
+  color: #30bf78;
+  font-weight: bold;
+}
+`);
 
 const gaugePlot = new Gauge(document.getElementById('container'), {
   title: {
@@ -13,6 +36,9 @@ const gaugePlot = new Gauge(document.getElementById('container'), {
   gaugeStyle: {
     colors: ['#f4664a', '#faad14', '#a0d911', '#30bf78'],
     tickLabelSize: 20,
+  },
+  label: () => {
+    return '<div class="g2plot-gauge-label"><p class="title">优</p><p class="value">系统表现</p></div>';
   },
 });
 gaugePlot.render();

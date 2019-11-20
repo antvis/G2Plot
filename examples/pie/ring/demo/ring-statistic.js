e95c821fccaf0dc2b868a80ad8f8966dc3094c30
paleface001 <mipha.ly@antfin.com>
2019-11-20 14:03:40 +0800

    ring chart centralText => statistic

--- examples/pie/ring/demo/ring-statistic.js
+++ examples/pie/ring/demo/ring-statistic.js
@@ -31,18 +31,21 @@ const ringPlot = new Ring(document.getElementById('container'), {
   forceFit: true,
   title: {
     visible: true,
-    text: '环图-中心文本',
+    text: '环图-指标卡',
   },
   description: {
     visible: true,
-    text: '环图中心文本能够代替tooltip，在环图中心挖空部分显示各分类的详细信息。',
+    text: '环图指标卡能够代替tooltip，在环图中心挖空部分显示各分类的详细信息。',
   },
   radius: 0.8,
   padding: 'auto',
   data,
   angleField: 'value',
   colorField: 'type',
-  annotation: [{ type: 'centralText', onActive: true }],
+  statistic: {
+    visible: true,
+    onActive: true,
+  },
   label: {
     offset: -22,
   },

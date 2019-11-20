75c2641e45275100c7ecb170bad92eef2cc3ba37
hustcc <i@hust.cc>
2019-11-20 13:54:06 +0800

    feat(demo): add description and label formatter for line, area

--- examples/area/basic/demo/slider.js
+++ examples/area/basic/demo/slider.js
@@ -8,6 +8,10 @@ fetch('../data/sales.json')
         visible: true,
         text: '基础面积图 - 缩略轴',
       },
+      description: {
+        visible: true,
+        text: '缩略轴 (slider) 交互适用于数据较多，用户希望关注数据集中某个特殊区间的场景。',
+      },
       data,
       xField: '城市',
       xAxis: {
@@ -15,6 +19,12 @@ fetch('../data/sales.json')
         autoHideLabel: true,
       },
       yField: '销售额',
+      yAxis: {
+        label: {
+          // 数值格式化为千分位
+          formatter: (v) => `${v}`.replace(/\d{1,3}(?=(\d{3})+$)/g, (s) => `${s},`),
+        },
+      },
       interactions: [
         {
           type: 'slider',

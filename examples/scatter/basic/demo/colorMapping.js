bb1254e4578672176e3e7e45e16676ae31ff5eb0
paleface001 <mipha.ly@antfin.com>
2019-12-14 15:53:06 +0800

    chore: 完善散点图demo

--- examples/scatter/basic/demo/colorMapping.js
+++ examples/scatter/basic/demo/colorMapping.js
@@ -6,11 +6,16 @@ fetch('../data/IMDB.json')
     const scatterPlot = new Scatter(document.getElementById('container'), {
       title: {
         visible: true,
-        text: '基础散点图',
+        text: '散点图颜色映射',
       },
       data,
       xField: 'Revenue (Millions)',
       yField: 'Rating',
+      colorField: 'Genre',
+      color: ['#d62728', '#2ca02c', '#000000', '#9467bd', '#ffd500', '#1f77b4', '#00518a', '#ffbc69', '#9bd646'],
+      pointStyle: {
+        fillOpacity: 1,
+      },
       xAxis: {
         visible: true,
         min: -5,

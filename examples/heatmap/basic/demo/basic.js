aa48449d60e75bcd0d0adbc5e163beaa114d9c50
paleface001 <mipha.ly@antfin.com>
2020-01-02 23:22:36 +0800

    fix: heatmap horizontal legend text position

--- examples/heatmap/basic/demo/basic.js
+++ examples/heatmap/basic/demo/basic.js
@@ -9,8 +9,18 @@ fetch('../data/jobpaying.json')
       yField: 'Average annual wage',
       colorField: 'numbEmployed',
       color: ['#295599', '#3e94c0', '#78c6d0', '#b4d9e4', '#fffef0', '#f9cdac', '#ec7d92', '#bc448c'],
-      radius: 20,
+      radius: 15,
       intensity: 2,
+      xAxis: {
+        visible: true,
+        min: -0.05,
+        max: 1.05,
+        nice: false,
+      },
+      yAxis: {
+        visible: true,
+        min: -1000,
+      },
     });
 
     heatMapPlot.render();

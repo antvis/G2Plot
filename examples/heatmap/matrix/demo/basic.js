940c5179105b9ef3debd98bb4bfbc56f8d1db991
paleface001 <mipha.ly@antfin.com>
2020-01-03 15:18:48 +0800

    feat: add matrix plot demo

---
+++ examples/heatmap/matrix/demo/basic.js
@@ -0,0 +1,23 @@
+import { Matrix } from '@antv/g2plot';
+
+fetch('https://gw.alipayobjects.com/os/basement_prod/a719cd4e-bd40-4878-a4b4-df8a6b531dfe.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const matrixPlot = new Matrix(document.getElementById('container'), {
+      forceFit: false,
+      width: 650,
+      height: 500,
+      data,
+      xField: 'Month of Year',
+      yField: 'District',
+      colorField: 'AQHI',
+      shapeType: 'rect',
+      color: ['#174c83', '#7eb6d4', '#efefeb', '#efa759', '#9b4d16'],
+      meta: {
+        'Month of Year': {
+          type: 'cat',
+        },
+      },
+    });
+    matrixPlot.render();
+  });

2a614d37fb54673c8fa3b782e98ad276d54e806f
paleface001 <mipha.ly@antfin.com>
2020-01-07 12:15:36 +0800

    fix: index error

--- src/plots/treemap/layout/slice.ts
+++ src/plots/treemap/layout/slice.ts
@@ -1,17 +1,18 @@
 import { each } from '@antv/util';
 
-export function slice(root, x, y, width, height) {
+export function slice(root, x0, y0, x1, y1) {
+  const height = y1 - y0;
   const { children, value } = root;
   children.sort((a, b) => {
     return b.value - a.value;
   });
   const k = height / value;
-  let node_y = y;
+  let node_y = y0;
   each(children, (c) => {
-    c.x = x;
-    c.width = width;
-    c.y = node_y;
-    c.height = c.value * k;
+    c.x0 = x0;
+    c.x1 = x1;
+    c.y0 = node_y;
     node_y += c.value * k;
+    c.y1 = c.y0 + c.value * k;
   });
 }

935eb7802df21da7b0d674f7e1210cfddaa694ee
paleface001 <mipha.ly@antfin.com>
2020-01-08 17:41:39 +0800

    chore: refactor layout methods

--- src/plots/treemap/layout/util/conflictList.ts
+++ src/plots/treemap/layout/util/conflictList.ts
@@ -1,3 +1,99 @@
+export class ConflictList {
+  public forFace: boolean;
+  public head: any;
+  constructor(forFace) {
+    this.forFace = forFace;
+    this.head = null;
+  }
+
+  public add(cln) {
+    // conflict node
+    if (this.head === null) {
+      this.head = cln;
+    } else {
+      if (this.forFace) {
+        this.head.prevv = cln;
+        cln.nextv = this.head;
+        this.head = cln;
+      } else {
+        this.head.prevf = cln;
+        cln.nextf = this.head;
+        this.head = cln;
+      }
+    }
+  }
+
+  public isEmpty() {
+    return this.head === null;
+  }
+
+  public fill(visible) {
+    if (this.forFace) {
+      return;
+    }
+    let curr = this.head;
+    while (curr !== null) {
+      visible.push(curr.face);
+      curr.face.marked = true;
+      curr = curr.nextf;
+    }
+  }
+
+  public removeAll() {
+    if (this.forFace) {
+      let curr = this.head;
+      do {
+        if (curr.prevf === null) {
+          if (curr.nextf === null) {
+            curr.vert.conflicts.head = null;
+          } else {
+            curr.nextf.prevf = null;
+            curr.vert.conflicts.head = curr.nextf;
+          }
+        } else {
+          if (curr.nextf != null) {
+            curr.nextf.prevf = curr.prevf;
+          }
+          curr.prevf.nextf = curr.nextf;
+        }
+        curr = curr.nextv;
+        if (curr != null) {
+          curr.prevv = null;
+        }
+      } while (curr != null);
+    } else {
+      let curr = this.head;
+      do {
+        if (curr.prevv == null) {
+          if (curr.nextv == null) {
+            curr.face.conflicts.head = null;
+          } else {
+            curr.nextv.prevv = null;
+            curr.face.conflicts.head = curr.nextv;
+          }
+        } else {
+          if (curr.nextv != null) {
+            curr.nextv.prevv = curr.prevv;
+          }
+          curr.prevv.nextv = curr.nextv;
+        }
+        curr = curr.nextf;
+        if (curr != null) curr.prevf = null;
+      } while (curr != null);
+    }
+  }
+
+  public getVertices() {
+    const list = [];
+    let curr = this.head;
+    while (curr !== null) {
+      list.push(curr.vert);
+      curr = curr.nextv;
+    }
+    return list;
+  }
+}
+
 export function ConflictListNode(face, vert) {
   this.face = face;
   this.vert = vert;
@@ -6,106 +102,3 @@ export function ConflictListNode(face, vert) {
   this.nextv = null;
   this.prevv = null;
 }
-
-// IN: boolean forFace
-export function ConflictList(forFace) {
-  this.forFace = forFace;
-  this.head = null;
-}
-
-// IN: ConflictListNode cln
-ConflictList.prototype.add = function(cln) {
-  if (this.head === null) {
-    this.head = cln;
-  } else {
-    if (this.forFace) {
-      // Is FaceList
-      this.head.prevv = cln;
-      cln.nextv = this.head;
-      this.head = cln;
-    } else {
-      // Is VertexList
-      this.head.prevf = cln;
-      cln.nextf = this.head;
-      this.head = cln;
-    }
-  }
-};
-
-ConflictList.prototype.isEmpty = function() {
-  return this.head === null;
-};
-
-// Array of faces visible
-ConflictList.prototype.fill = function(visible) {
-  if (this.forFace) {
-    return;
-  }
-  let curr = this.head;
-  do {
-    visible.push(curr.face);
-    curr.face.marked = true;
-    curr = curr.nextf;
-  } while (curr !== null);
-};
-
-ConflictList.prototype.removeAll = function() {
-  if (this.forFace) {
-    // Remove all vertices from Face
-    let curr = this.head;
-    do {
-      if (curr.prevf === null) {
-        // Node is head
-        if (curr.nextf === null) {
-          curr.vert.conflicts.head = null;
-        } else {
-          curr.nextf.prevf = null;
-          curr.vert.conflicts.head = curr.nextf;
-        }
-      } else {
-        // Node is not head
-        if (curr.nextf != null) {
-          curr.nextf.prevf = curr.prevf;
-        }
-        curr.prevf.nextf = curr.nextf;
-      }
-      curr = curr.nextv;
-      if (curr != null) {
-        curr.prevv = null;
-      }
-    } while (curr != null);
-  } else {
-    // Remove all JFaces from vertex
-    let curr = this.head;
-    do {
-      if (curr.prevv == null) {
-        // Node is head
-        if (curr.nextv == null) {
-          curr.face.conflicts.head = null;
-        } else {
-          curr.nextv.prevv = null;
-          curr.face.conflicts.head = curr.nextv;
-        }
-      } else {
-        // Node is not head
-        if (curr.nextv != null) {
-          curr.nextv.prevv = curr.prevv;
-        }
-        curr.prevv.nextv = curr.nextv;
-      }
-      curr = curr.nextf;
-      if (curr != null) curr.prevf = null;
-    } while (curr != null);
-  }
-};
-
-// IN: list of vertices
-ConflictList.prototype.getVertices = function() {
-  const list = [];
-  let curr = this.head;
-  while (curr !== null) {
-    list.push(curr.vert);
-    curr = curr.nextv;
-  }
-  return list;
-};
--- src/plots/treemap/layout/util/vertex.ts
+++ src/plots/treemap/layout/util/vertex.ts
@@ -1,53 +1,65 @@
-// Vertex
 import { epsilon } from './assist';
 import { ConflictList } from './conflictList';
 
-// IN: coordinates x, y, z
-export function Vertex(x, y, z, weight?, orig?, isDummy?) {
-  this.x = x;
-  this.y = y;
-  this.weight = epsilon;
-  this.index = 0;
-  this.conflicts = new ConflictList(false);
-  this.neighbours = null; // Potential trouble
-  this.nonClippedPolygon = null;
-  this.polygon = null;
-  this.originalObject = null;
-  this.isDummy = false;
+export class Vertex {
+  public x: number;
+  public y: number;
+  public z: number;
+  public weight: number;
+  public index: number;
+  public conflicts: any;
+  public neighbours: any;
+  public nonClippedPolygon: any;
+  public polygon: any;
+  public originalObject: any;
+  public isDummy: boolean;
 
-  if (orig !== undefined) {
-    this.originalObject = orig;
+  constructor(x, y, z, weight?, orig?, isDummy?) {
+    this.x = x;
+    this.y = y;
+    this.weight = epsilon;
+    this.index = 0;
+    this.conflicts = new ConflictList(false);
+    this.neighbours = null; // Potential trouble
+    this.nonClippedPolygon = null;
+    this.polygon = null;
+    this.originalObject = null;
+    this.isDummy = false;
+
+    if (orig !== undefined) {
+      this.originalObject = orig;
+    }
+    if (isDummy != undefined) {
+      this.isDummy = isDummy;
+    }
+    if (weight != null) {
+      this.weight = weight;
+    }
+    if (z != null) {
+      this.z = z;
+    } else {
+      this.z = this.projectZ(this.x, this.y, this.weight);
+    }
   }
-  if (isDummy != undefined) {
-    this.isDummy = isDummy;
+
+  public projectZ(x, y, weight) {
+    return x * x + y * y - weight;
   }
-  if (weight != null) {
+
+  public setWeight(weight) {
     this.weight = weight;
-  }
-  if (z != null) {
-    this.z = z;
-  } else {
     this.z = this.projectZ(this.x, this.y, this.weight);
   }
+
+  public subtract(v) {
+    return new Vertex(v.x - this.x, v.y - this.y, v.z - this.z);
+  }
+
+  public crossproduct(v) {
+    return new Vertex(this.y * v.z - this.z * v.y, this.z * v.x - this.x * v.z, this.x * v.y - this.y * v.x);
+  }
+
+  public equals(v) {
+    return this.x === v.x && this.y === v.y && this.z === v.z;
+  }
 }
-
-Vertex.prototype.projectZ = function(x, y, weight) {
-  return x * x + y * y - weight;
-};
-
-Vertex.prototype.setWeight = function(weight) {
-  this.weight = weight;
-  this.z = this.projectZ(this.x, this.y, this.weight);
-};
-
-Vertex.prototype.subtract = function(v) {
-  return new Vertex(v.x - this.x, v.y - this.y, v.z - this.z);
-};
-
-Vertex.prototype.crossproduct = function(v) {
-  return new Vertex(this.y * v.z - this.z * v.y, this.z * v.x - this.x * v.z, this.x * v.y - this.y * v.x);
-};
-
-Vertex.prototype.equals = function(v) {
-  return this.x === v.x && this.y === v.y && this.z === v.z;
-};

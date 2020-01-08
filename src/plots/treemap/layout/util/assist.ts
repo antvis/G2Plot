fa82399a52e0ee5532bf12ecc0da75ad3daa0bde
paleface001 <mipha.ly@antfin.com>
2020-01-08 16:53:17 +0800

    chore: refractor layout & face

--- src/plots/treemap/layout/util/assist.ts
+++ src/plots/treemap/layout/util/assist.ts
@@ -4,15 +4,10 @@ export function epsilonesque(n) {
   return n <= epsilon && n >= -epsilon;
 }
 
-// IN: vectors or vertices
-// OUT: dot product
 export function dot(v0, v1) {
   return v0.x * v1.x + v0.y * v1.y + v0.z * v1.z;
 }
 
-// IN: two vertex objects, v0 and v1
-// OUT: true if they are linearly dependent, false otherwise
-// from https://math.stackexchange.com/questions/1144357/how-can-i-prove-that-two-vectors-in-%E2%84%9D3-are-linearly-independent-iff-their-cro
 export function linearDependent(v0, v1) {
   return (
     epsilonesque(v0.x * v1.y - v0.y * v1.x) &&
@@ -21,35 +16,29 @@ export function linearDependent(v0, v1) {
   );
 }
 
-// IN: an array of 2D-points [x,y]
-// OUT: true if the set defines a convex polygon (non-intersecting, hole-free, non-concave)
-// from https://gist.github.com/annatomka/82715127b74473859054, adapted to [x,y] syntax (instead of {x: ..., y: ...}) and optimizations
 export function polygonDirection(polygon) {
   let direction, crossproduct, p0, p1, p2, v0, v1, i;
 
-  //begin: initialization
   p0 = polygon[polygon.length - 2];
   p1 = polygon[polygon.length - 1];
   p2 = polygon[0];
   v0 = vect(p0, p1);
   v1 = vect(p1, p2);
   crossproduct = calculateCrossproduct(v0, v1);
-  // console.log(`[[${p0}], [${p1}], [${p2}]] => (${v0}) x (${v1}) = ${crossproduct}`);
-  const sign = Math.sign(crossproduct);
-  //end: initialization
 
-  p0 = p1; // p0 = polygon[polygon.length - 1];
-  p1 = p2; // p1 = polygon[0];
+  const sign = Math.sign(crossproduct);
+
+  p0 = p1;
+  p1 = p2;
   p2 = polygon[1];
   v0 = v1;
   v1 = vect(p1, p2);
   crossproduct = calculateCrossproduct(v0, v1);
-  // console.log(`[[${p0}], [${p1}], [${p2}]] => (${v0}) x (${v1}) = ${crossproduct}`);
+
   if (Math.sign(crossproduct) !== sign) {
     return undefined;
-  } //different signs in cross products means concave polygon
+  }
 
-  //iterate on remaining 3 consecutive points
   for (i = 2; i < polygon.length - 1; i++) {
     p0 = p1;
     p1 = p2;
@@ -57,10 +46,10 @@ export function polygonDirection(polygon) {
     v0 = v1;
     v1 = vect(p1, p2);
     crossproduct = calculateCrossproduct(v0, v1);
-    // console.log(`[[${p0}], [${p1}], [${p2}]] => (${v0}) x (${v1}) = ${crossproduct}`);
+
     if (Math.sign(crossproduct) !== sign) {
       return undefined;
-    } //different signs in cross products means concave polygon
+    }
   }
 
   return sign;
--- src/plots/treemap/layout/util/face.ts
+++ src/plots/treemap/layout/util/face.ts
@@ -4,105 +4,106 @@ import { ConflictList } from './conflictList';
 import { Vector } from './vector';
 import { HEdge } from './hEdges';
 
-// IN: Vertices a, b, c
-export function Face(a, b, c, orient?) {
-  this.conflicts = new ConflictList(true);
-  this.verts = [a, b, c];
-  this.marked = false;
-  const t = a.subtract(b).crossproduct(b.subtract(c));
+export class Face {
+  public conflicts: any;
+  public verts: any[];
+  public edges: any[];
+  public marked: boolean;
+  private normal: any;
+  private dualPoint: any;
 
-  this.normal = new Vector(-t.x, -t.y, -t.z);
-  this.normal.normalize();
-  this.createEdges();
-  this.dualPoint = null;
+  constructor(a, b, c, orient?) {
+    this.conflicts = new ConflictList(true);
+    this.verts = [a, b, c];
+    this.marked = false;
+    const t = a.subtract(b).crossproduct(b.subtract(c));
+    this.normal = new Vector(-t.x, -t.y, -t.z);
+    this.normal.normalize();
+    this.createEdges();
+    this.dualPoint = null;
+    if (orient != undefined) {
+      this.orient(orient);
+    }
+  }
 
-  if (orient != undefined) {
-    this.orient(orient);
+  public getDualPoint() {
+    if (this.dualPoint == null) {
+      const plane3d = new Plane3D(this);
+      this.dualPoint = plane3d.getDualPointMappedToPlane();
+    }
+    return this.dualPoint;
+  }
+
+  public isVisibleFromBelow() {
+    return this.normal.z < -1.4259414393190911e-9;
+  }
+
+  public createEdges() {
+    this.edges = [];
+    this.edges[0] = new HEdge(this.verts[0], this.verts[1], this);
+    this.edges[1] = new HEdge(this.verts[1], this.verts[2], this);
+    this.edges[2] = new HEdge(this.verts[2], this.verts[0], this);
+    this.edges[0].next = this.edges[1];
+    this.edges[0].prev = this.edges[2];
+    this.edges[1].next = this.edges[2];
+    this.edges[1].prev = this.edges[0];
+    this.edges[2].next = this.edges[0];
+    this.edges[2].prev = this.edges[1];
+  }
+
+  public orient(orient) {
+    if (!(dot(this.normal, orient) < dot(this.normal, this.verts[0]))) {
+      const temp = this.verts[1];
+      this.verts[1] = this.verts[2];
+      this.verts[2] = temp;
+      this.normal.negate();
+      this.createEdges();
+    }
+  }
+
+  public getEdge(v1, v0) {
+    for (let i = 0; i < 3; i++) {
+      if (this.edges[i].isEqual(v0, v1)) {
+        return this.edges[i];
+      }
+    }
+    return null;
+  }
+
+  public link(face, v0, v1) {
+    if (face instanceof Face) {
+      const twin = face.getEdge(v0, v1);
+      if (twin === null) {
+        return;
+      }
+      const edge = this.getEdge(v0, v1);
+      if (edge === null) {
+        return;
+      }
+      twin.twin = edge;
+      edge.twin = twin;
+    } else {
+      const twin = face;
+      const edge = this.getEdge(twin.orig, twin.dest);
+      twin.twin = edge;
+      edge.twin = twin;
+    }
+  }
+
+  public conflict(v) {
+    return dot(this.normal, v) > dot(this.normal, this.verts[0]) + epsilon;
+  }
+
+  public getHorizon() {
+    for (let i = 0; i < 3; i++) {
+      if (this.edges[i].twin !== null && this.edges[i].twin.isHorizon()) {
+        return this.edges[i];
+      }
+    }
+    return null;
+  }
+
+  public removeConflict() {
+    this.conflicts.removeAll();
   }
 }
-
-// OUT: Point2D
-Face.prototype.getDualPoint = function() {
-  if (this.dualPoint == null) {
-    const plane3d = new Plane3D(this);
-    this.dualPoint = plane3d.getDualPointMappedToPlane();
-  }
-  return this.dualPoint;
-};
-
-Face.prototype.isVisibleFromBelow = function() {
-  return this.normal.z < -1.4259414393190911e-9;
-};
-
-Face.prototype.createEdges = function() {
-  this.edges = [];
-  this.edges[0] = new HEdge(this.verts[0], this.verts[1], this);
-  this.edges[1] = new HEdge(this.verts[1], this.verts[2], this);
-  this.edges[2] = new HEdge(this.verts[2], this.verts[0], this);
-  this.edges[0].next = this.edges[1];
-  this.edges[0].prev = this.edges[2];
-  this.edges[1].next = this.edges[2];
-  this.edges[1].prev = this.edges[0];
-  this.edges[2].next = this.edges[0];
-  this.edges[2].prev = this.edges[1];
-};
-
-// IN: vertex orient
-Face.prototype.orient = function(orient) {
-  if (!(dot(this.normal, orient) < dot(this.normal, this.verts[0]))) {
-    const temp = this.verts[1];
-    this.verts[1] = this.verts[2];
-    this.verts[2] = temp;
-    this.normal.negate();
-    this.createEdges();
-  }
-};
-
-// IN: two vertices v0 and v1
-Face.prototype.getEdge = function(v0, v1) {
-  for (let i = 0; i < 3; i++) {
-    if (this.edges[i].isEqual(v0, v1)) {
-      return this.edges[i];
-    }
-  }
-  return null;
-};
-
-// IN: Face face, vertices v0 and v1
-Face.prototype.link = function(face, v0, v1) {
-  if (face instanceof Face) {
-    const twin = face.getEdge(v0, v1);
-    if (twin === null) {
-      console.log('ERROR: when linking, twin is null');
-    }
-    const edge = this.getEdge(v0, v1);
-    if (edge === null) {
-      console.log('ERROR: when linking, edge is null');
-    }
-    twin.twin = edge;
-    edge.twin = twin;
-  } else {
-    const twin = face; // face is a hEdge
-    const edge = this.getEdge(twin.orig, twin.dest);
-    twin.twin = edge;
-    edge.twin = twin;
-  }
-};
-
-// IN: vertex v
-Face.prototype.conflict = function(v) {
-  return dot(this.normal, v) > dot(this.normal, this.verts[0]) + epsilon;
-};
-
-Face.prototype.getHorizon = function() {
-  for (let i = 0; i < 3; i++) {
-    if (this.edges[i].twin !== null && this.edges[i].twin.isHorizon()) {
-      return this.edges[i];
-    }
-  }
-  return null;
-};
-
-Face.prototype.removeConflict = function() {
-  this.conflicts.removeAll();
-};

0156e7a09b7961b84e3328f327e564bd4f5ca1a4
paleface001 <mipha.ly@antfin.com>
2020-01-10 01:36:10 +0800

    feat: add iterator

---
+++ src/plots/treemap/layout/util/half-average-weight.ts
@@ -0,0 +1,7 @@
+import { area } from './polygon';
+
+export function halfAverageArea(cliper,data) {
+    const num = data.length;
+    const totalArea = area(cliper);
+    return totalArea / num / 2;
+}

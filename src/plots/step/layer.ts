474612dfe0b61adcff062ac91e818b71d05194ae
hustcc <i@hust.cc>
2020-01-10 10:57:22 +0800

    feat(step): 添加 step 图组件和配置类型定义

---
+++ src/plots/step/layer.ts
@@ -0,0 +1,20 @@
+import LineLayer, { LineLayerConfig, LineViewConfig } from '../line/layer';
+import { LayerConfig } from '../..';
+import { registerPlotType } from '../../base/global';
+import * as _ from '@antv/util';
+
+export interface StepViewConfig extends LineViewConfig {
+  readonly step?: 'hv' | 'vh' | 'vhv' | 'hvh'; // 默认为 hv
+}
+
+export interface StepLayerConfig extends StepViewConfig, LayerConfig {}
+
+export class StepLayer extends LineLayer<StepLayerConfig> {
+  public static getDefaultOptions(): Partial<LineLayerConfig> {
+    return _.deepMix({}, super.getDefaultOptions(), {
+      step: 'hv',
+    });
+  }
+}
+
+registerPlotType('step', StepLayer);

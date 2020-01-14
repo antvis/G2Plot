b1826eb4baa22a3733e58970be184d032e9ce721
brucetoo <tuyong.ty@alibaba-inc.com>
2020-01-14 19:57:51 +0800

    feat(word-cloud): fix g2 tempTpl li default dot style, replace with div

--- src/plots/word-cloud/word-cloud-tooltips.ts
+++ src/plots/word-cloud/word-cloud-tooltips.ts
@@ -2,12 +2,18 @@
  * Create By Bruce Too
  * On 2020-01-09
  */
+import * as _ from '@antv/util';
 import HtmlTooltip from '@antv/component/lib/tooltip/html';
 import { TooltipCfg } from '@antv/component/lib/tooltip/interface';
 
 export default class WordCloudTooltips extends HtmlTooltip {
   constructor(cfg: TooltipCfg) {
-    super(cfg);
+    const newCfg = _.deepMix({}, cfg, {
+      itemTpl: `<div data-index={index}>
+        <span style="background-color:{color};" class="g2-tooltip-marker"></span>
+        {name}<span class="g2-tooltip-value">{value}</span></div>`,
+    });
+    super(newCfg);
   }
 
   public show() {

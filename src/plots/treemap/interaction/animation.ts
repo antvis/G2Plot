8b85beb8c7e4bf63cb05cd41a5f665b707f6c179
paleface001 <mipha.ly@antfin.com>
2020-01-20 17:31:50 +0800

    feat: drilldown animation

--- src/plots/treemap/interaction/animation.ts
+++ src/plots/treemap/interaction/animation.ts
@@ -1,6 +1,7 @@
-import { each } from '@antv/util';
+import { each, clone, deepMix } from '@antv/util';
 
 const ulMatrix = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+const duration = 300;
 
 export function scale(target, container, view, callback) {
   const path = target.attr('path');
@@ -37,28 +38,59 @@ export function scale(target, container, view, callback) {
       ['t', offsetX, offsetY],
     ],
   };
-
-  const geometry = view.get('elements')[0];
-  const shapes = geometry.getShapes();
-
-  each(shapes, (shape, index) => {
-    if (index === 0) {
-      shape.animate(transform, 300, () => {
-        triggerCallback(container, callback);
-      });
-    } else {
-      shape.animate(transform, 300);
-    }
-  });
-
-  /*container.animate(transform,300,()=>{
-        triggerCallback(container,callback);
-    });*/
+  let geometry = view.get('elements')[0];
+  const tem_container = view.get('container').addGroup();
+  tem_container.set('zIndex', -100);
+  const tem_shapes = getTemShapes(geometry, tem_container);
+  geometry.get('container').set('visible', false);
+  view.get('canvas').draw();
+  callback();
+  window.setTimeout(() => {
+    each(tem_shapes, (shape, index) => {
+      if (index === 0) {
+        shape.animate(transform, duration, () => {
+          tem_container.remove();
+          view.get('canvas').draw();
+        });
+      } else {
+        shape.animate(transform, duration);
+      }
+    });
+    geometry = view.get('elements')[0];
+    const shapes = geometry.getShapes();
+    each(shapes, (shape) => {
+      shape.attr('opacity', 0);
+      shape.animate(
+        {
+          opacity: 1,
+        },
+        duration
+      );
+    });
+    geometry.get('container').set('visible', true);
+    geometry.get('container').transform([
+      ['s', rect.width / range.width, rect.height / range.height],
+      ['t', rect.minX, rect.minY],
+    ]);
+    const matrix = clone(ulMatrix);
+    geometry.get('container').animate(
+      {
+        matrix,
+      },
+      duration
+    );
+    view.get('canvas').draw();
+  }, 40);
 }
 
-function triggerCallback(container, callback) {
-  window.setTimeout(() => {
-    callback();
-    //container.attr('matrix',ulMatrix);
-  }, 200);
+function getTemShapes(geometry, container) {
+  const shapes = geometry.getShapes();
+  const tem_shapes = [];
+  each(shapes, (shape) => {
+    const s = container.addShape('path', {
+      attrs: deepMix({}, shape.attrs, { capture: false }),
+    });
+    tem_shapes.push(s);
+  });
+  return tem_shapes;
 }

6b2b128abbe75987dac0b147dfbebc4d1adf20b9
paleface001 <mipha.ly@antfin.com>
2020-02-05 17:02:13 +0800

    feat: add label

--- src/plots/ranged-bar/component/label.ts
+++ src/plots/ranged-bar/component/label.ts
@@ -15,6 +15,109 @@ export interface RangedBarLabelConfig {
 }
 
 export interface IRangedBarLabel extends RangedBarLabelConfig {
+    view: View;
     plot: any;
 }
 
+
+export default class RangedBarLabel {
+    public options: RangedBarLabelConfig;
+    private plot: any;
+    private view : View;
+    private container: Group;
+
+    constructor(cfg: IRangedBarLabel) {
+        const defaultOptions = this.getDefaultOptions();
+        this.options = deepMix({}, defaultOptions, cfg);
+        this.view = cfg.view;
+        this.plot = cfg.plot;
+        this.init();
+    }
+
+    public init(){
+        const geomContainer = this.view.get('elements')[0].get('container');
+        this.container = geomContainer.addGroup();
+    }
+
+    public render(){
+        const shapes = this.view.get('elements')[0].getShapes();
+        each(shapes,(shape)=>{
+            const positions = this.getPosition(shape);
+            const values = this.getValue(shape);
+            const textAlign = this.getTextAlign();
+            each(positions,(pos,i)=>{
+                const text = this.container.addShape('text',{
+                    attrs:{
+                        x:pos.x,
+                        y:pos.y,
+                        text:values[i],
+                        fontSize: 12,
+                        fill:'#000000',
+                        textAlign:textAlign[i],
+                        textBaseline:'middle'
+                    }
+                });
+            })
+        });
+
+        this.plot.canvas.draw();
+    }
+
+    public hide(){
+
+    }
+
+    public show(){
+
+    }
+
+    public clear(){
+
+    }
+
+    public destory(){
+
+    }
+
+    public getBBox(){
+
+    }
+
+    private getDefaultOptions(){
+        return {
+
+        };
+    }
+
+    private getPosition(shape){
+        const bbox = shape.getBBox();
+        const offset = 8;
+        const y = bbox.minY + bbox.height / 2;
+        let x1,x2;
+        if(this.options.position === 'outer'){
+            x1 = bbox.minX - offset;
+            x2 = bbox.maxX + offset;
+        }else{
+            x1 = bbox.minX + offset;
+            x2 = bbox.maxX - offset;
+        }
+
+        return [
+            {x:x1,y},
+            {x:x2,y}
+        ];
+    }
+
+    private getValue(shape){
+        const { xField } = this.plot.options;
+        return shape.get('origin')._origin[xField];
+    }
+
+    private getTextAlign(){
+       if(this.options.position === 'outer'){
+           return ['right','left'];
+       }else{
+           return ['left','right'];
+       }
+    }
+}
--- src/plots/ranged-bar/layer.ts
+++ src/plots/ranged-bar/layer.ts
@@ -2,21 +2,34 @@ import * as _ from '@antv/util';
 import { registerPlotType } from '../../base/global';
 import { LayerConfig } from '../../base/layer';
 import BaseBarLayer, { BarViewConfig } from '../bar/layer';
+import RangedBarLabel, { RangedBarLabelConfig } from './component/label';
 
 export interface RangedBarViewConfig extends BarViewConfig {
     data: any; // todo: 补上data类型定义
+    label: RangedBarLabelConfig
 }
 
 export interface RangedBarLayerConfig extends RangedBarViewConfig, LayerConfig {}
 
 export default class RangedBarLayer extends BaseBarLayer<RangedBarLayerConfig> {
-  public static getDefaultOptions(): Partial<RangedBarViewConfig> {
+  /*public static getDefaultOptions(): Partial<RangedBarViewConfig> {
     return _.deepMix({
-       
     }, super.getDefaultOptions(), {});
-  }
+  }*/
 
   public type: string = 'rangedBar';
+
+  public afterRender(){
+      if(this.options.label && this.options.label.visible){
+          const label = new RangedBarLabel({
+              view: this.view,
+              plot: this,
+              ...this.options.label
+          });
+          label.render();
+      }
+      super.afterRender();
+  }
 
   protected extractLabel(){}
 }

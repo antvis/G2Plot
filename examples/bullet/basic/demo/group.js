a800cb01fb87d9f80ac046a42de8acd664816904
Kasmine <736929286@qq.com>
2020-02-21 11:20:58 +0800

    feat: bullet (#579)
    * feat(bullet): 支持子弹图
    
    * feat(bullet): 子弹图网站demo & 截图 & api 文档
    
    * feat(bullet): 子弹图单测
    
    * fix(bullet): 子弹图相关组件增加destroy方法
    
    * fix(bullet): rebase master & remove unused file
    
    * fix(bullet): fix prettier error

---
+++ examples/bullet/basic/demo/group.js
@@ -0,0 +1,39 @@
+import { Bullet } from '@antv/g2plot';
+
+const bulletPlot = new Bullet(document.getElementById('container'), {
+  data: [
+    {
+      title: '广州',
+      measures: [83],
+      targets: [90],
+    },
+    {
+      title: '深圳',
+      measures: [13],
+      targets: [90],
+    },
+    {
+      title: '珠海',
+      measures: [45],
+      targets: [80],
+    },
+    {
+      title: '汕头',
+      measures: [83],
+      targets: [70],
+    },
+  ],
+  rangeMax: 100,
+
+  title: {
+    visible: true,
+    text: '分组子弹图',
+  },
+  description: {
+    visible: true,
+    text:
+      '当data数组由多个值时，可以展示多条子弹图进行进度对比',
+  },
+});
+
+bulletPlot.render();

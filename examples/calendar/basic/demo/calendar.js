4dab1227148b149ecedfe7c3541e1d9dedc49c6f
hustcc <i@hust.cc>
2020-02-25 17:31:18 +0800

    feat(calendar): 增加日历图 example

---
+++ examples/calendar/basic/demo/calendar.js
@@ -0,0 +1,26 @@
+import { Calendar } from '@antv/g2plot';
+
+fetch('../data/contributions.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const calendar = new Calendar(document.getElementById('container'), {
+      // title: {
+      //   visible: true,
+      //   text: 'GitHub contribution',
+      // },
+      // description: {
+      //   visible: true,
+      //   text: '853 contributions in the last year.',
+      // },
+      width: 650,
+      height: 250,
+      data,
+      dateField: 'date',
+      valueField: 'commits',
+      dateRange: ['2017-05-01', '2017-10-31'],
+      colors: '#BAE7FF-#1890FF-#0050B3',
+      padding: 'auto',
+    });
+
+    calendar.render();
+  });

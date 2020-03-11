21a203c996275ce929f421c79ee22cf11f07912f
zqlu <zqlu.cn@gmail.com>
2020-03-11 11:20:02 +0800

    feat: support custom widthRatio

--- src/geoms/base.ts
+++ src/geoms/base.ts
@@ -1,4 +1,4 @@
-import * as _ from '@antv/util';
+import { assign, deepMix, get } from '@antv/util';
 import { ElementOption } from '../interface/config';
 
 export default class ElementParser {
@@ -7,10 +7,10 @@ export default class ElementParser {
   public config: ElementOption;
   public style: any;
   private positionFields: string[];
-  private widthRatio: number;
+  private widthRatio: { [key: string]: number };
 
   constructor(cfg) {
-    _.assign(this, cfg);
+    assign(this, cfg);
     this.init();
   }
 
@@ -20,12 +20,16 @@ export default class ElementParser {
       position: {
         fields: this.positionFields,
       },
-      widthRatio: {
-        // 宽度所占的分类的比例
-        column: this.widthRatio || 3 / 5, // g2plot定制
-        rose: 0.9999999, // 玫瑰图柱状占比 1
-        multiplePie: 1 / 1.3, // 多层的饼图、环图
-      },
+      widthRatio: deepMix(
+        {},
+        {
+          column: 3 / 5,
+          rose: 0.9999999, // 玫瑰图柱状占比 1
+          multiplePie: 1 / 1.3, // 多层的饼图、环图
+        },
+        this.plot.options?.widthRatio || {},
+        this.widthRatio || {}
+      ),
     };
   }
 }

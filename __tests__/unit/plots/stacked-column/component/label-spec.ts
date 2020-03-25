b14b21ddd6656778b38efb21d2bc24ea881ada87
zqlu <zqlu.cn@gmail.com>
2020-03-25 10:35:11 +0800

    fix: unit test fix

--- __tests__/unit/plots/stacked-column/component/label-spec.ts
+++ __tests__/unit/plots/stacked-column/component/label-spec.ts
@@ -17,37 +17,39 @@ const config: StackColumnConfig = {
 };
 
 describe('StackedColumn Label', () => {
-  const plot = new StackedColumn(createDiv(), {
-    ...config,
-    label: {
-      visible: true,
-    },
-    meta: {
-      sales: {
-        formatter: (v) => Number(v).toFixed(2),
+  it('adjust', () => {
+    const plot = new StackedColumn(createDiv(), {
+      ...config,
+      label: {
+        visible: true,
       },
-    },
-  });
-  plot.render();
+      meta: {
+        sales: {
+          formatter: (v) => Number(v).toFixed(2),
+        },
+      },
+    });
+    plot.render();
 
-  const view = plot.getView();
-  const intervals = getGeometryShapes(getGeometryByType(view, 'interval'));
-  const labels = plot.getLayer().getLabels();
-  const labelShapes = labels[0] && labels[0].getLabels();
+    const view = plot.getView();
+    const intervals = getGeometryShapes(getGeometryByType(view, 'interval'));
+    const labels = plot.getLayer().getLabels();
+    const labelShapes = labels[0] && labels[0].getLabels();
 
-  // Label 组件
-  expect(labels).toHaveLength(1);
+    // Label 组件
+    expect(labels).toHaveLength(1);
 
-  // Label Shape
-  expect(labelShapes).toHaveLength(data.length);
-  each(intervals, (interval, idx) => {
-    expect(labelShapes[idx].attr('text')).toBe(labelShapes[idx].get('origin')['sales'].toFixed(2));
-  });
+    // Label Shape
+    expect(labelShapes).toHaveLength(data.length);
+    each(intervals, (interval, idx) => {
+      expect(labelShapes[idx].attr('text')).toBe(labelShapes[idx].get('origin')['sales'].toFixed(2));
+    });
 
-  // adjust
-  each(labelShapes, (shape: IShape, idx) => {
-    const intervalRange = intervals[idx].getBBox();
-    const shapeRange = shape.getBBox();
-    expect(shape.get('visible')).toBe(shapeRange.height <= intervalRange.height);
+    // adjust
+    each(labelShapes, (shape: IShape, idx) => {
+      const intervalRange = intervals[idx].getBBox();
+      const shapeRange = shape.getBBox();
+      expect(shape.get('visible')).toBe(shapeRange.height <= intervalRange.height);
+    });
   });
 });

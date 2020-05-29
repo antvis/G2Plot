75645740446237e79f98f55777b9db9a545aab48
Jingsong Gao <kingsongao1221@gmail.com>
2020-05-29 17:16:56 +0800

    fix(types): fix Donut statistic type definitions (#1149)
    * fix(types): fix Donut statistic type definitions
    
    - mark `totalLabel` as optional
    - mark `triggerOn` as optional
    - mark `triggerOff` as optional
    
    * fix(types): add content interface

--- src/plots/donut/component/ring-statistic.ts
+++ src/plots/donut/component/ring-statistic.ts
@@ -1,11 +1,11 @@
 import { View } from '../../../dependents';
-import StatisticHtml, { IStaticticHtml } from './statistic';
+import StatisticHtml, { IStatisticHtml } from './statistic';
 import * as statisticTemplate from './statistic-template';
 import { debounce, each, isString, isObject, isFunction, keys } from '@antv/util';
 import { LooseMap } from '../../../interface/types';
 import Ring, { DonutViewConfig } from '../layer';
 
-interface IRingStatictic extends IStaticticHtml {
+interface IRingStatistic extends IStatisticHtml {
   view: View;
   plot: any;
 }
@@ -16,7 +16,7 @@ export default class RingStatistic extends StatisticHtml {
   protected statisticClass: string;
   protected options: DonutViewConfig['statistic'];
 
-  constructor(cfg: IRingStatictic) {
+  constructor(cfg: IRingStatistic) {
     super(cfg);
     this.view = cfg.view;
     this.plot = cfg.plot;

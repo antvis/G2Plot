5abe0d99bfcfe83dd11644d7bdada0f52c70df2e
xinming <xinming.lxj@antfin.com>
2020-07-14 20:37:05 +0800

    feat(common): 抽取共同 adaptor 构建器
    [x] 支持 tooltip

---
+++ src/common/adaptor.ts
@@ -0,0 +1,23 @@
+/**
+ * @file 通用的一些 adaptor
+ */
+import { Params } from '../core/adaptor';
+import { Options } from '../types';
+
+/**
+ * 通用 tooltip 配置
+ * @param params
+ */
+function tooltip<O extends Options>(params: Params<O>): Params<O> {
+  const { chart, options } = params;
+  const { tooltip } = options;
+
+  if (tooltip) {
+    chart.tooltip(tooltip);
+  }
+
+  return params;
+}
+
+
+export { tooltip };

50abb281d16d413f648d7ce7adc17d6adfd1f478
yinshi.wyl <yinshi.wyl@antfin.com>
2020-07-20 20:10:36 +0800

    feat: add tiny-line

--- src/plots/sparkline/tiny-line/index.ts
+++ src/plots/sparkline/tiny-line/index.ts
@@ -10,7 +10,7 @@ export class TinyLine extends Plot<TinyLineOptions> {
   public type: string = 'tiny-line';
 
   /**
-   * 获取 折线图 的适配器
+   * 获取 迷你折线图 的适配器
    */
   protected getSchemaAdaptor(): Adaptor<TinyLineOptions> {
     return adaptor;

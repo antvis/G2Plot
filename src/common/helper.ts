5b90c6b1b6ff4dd544ce5707499559ad60586ec9
zqlu <zqlu.cn@gmail.com>
2020-07-22 11:31:47 +0800

    feat(v2/column): simplify findGeometry signature

--- src/common/helper.ts
+++ src/common/helper.ts
@@ -4,6 +4,6 @@ import { Chart, Geometry } from '@antv/g2';
  * 在 Chart 中查找第一个指定 type 类型的 geometry
  * @param chart
  */
-export function findGeometry(chart: Chart, type: string): Geometry | undefined {
+export function findGeometry(chart: Chart, type: string): Geometry {
   return chart.geometries.find((g: Geometry) => g.type === type);
 }

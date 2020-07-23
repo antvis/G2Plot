186db3575b220035568e41a33975fb44b1f8b3ee
羽熙 <yuxi.lx@antfin.com>
2020-07-23 15:35:58 +0800

    feat(v2/bar): add basic bar chart

---
+++ __tests__/unit/plots/bar/axis-spec.ts
@@ -0,0 +1,75 @@
+import { Bar } from '../../../../src';
+import { salesByArea } from '../../../data/sales';
+import { createDiv } from '../../../utils/dom';
+
+describe('bar axis', () => {
+  it('meta', () => {
+    const formatter = (v) => `${Math.floor(v / 10000)}万`;
+    const bar = new Bar(createDiv(), {
+      width: 400,
+      height: 300,
+      data: salesByArea,
+      xField: 'sales',
+      yField: 'area',
+      meta: {
+        sales: {
+          nice: true,
+          formatter,
+        },
+      },
+    });
+
+    bar.render();
+
+    const geometry = bar.chart.geometries[0];
+    // @ts-ignore
+    expect(geometry.scales.sales.nice).toBe(true);
+    expect(geometry.scales.sales.formatter).toBe(formatter);
+  });
+
+  it('xAxis', () => {
+    const bar = new Bar(createDiv(), {
+      width: 400,
+      height: 300,
+      data: salesByArea,
+      xField: 'sales',
+      yField: 'area',
+      xAxis: {
+        minLimit: 10000,
+        nice: true,
+      },
+    });
+
+    bar.render();
+
+    const geometry = bar.chart.geometries[0];
+    const axisOptions = bar.chart.getOptions().axes;
+
+    // @ts-ignore
+    expect(axisOptions.sales.minLimit).toBe(10000);
+    expect(geometry.scales.sales.minLimit).toBe(10000);
+    // @ts-ignore
+    expect(geometry.scales.sales.nice).toBe(true);
+  });
+
+  it('xAxis', () => {
+    const bar = new Bar(createDiv(), {
+      width: 400,
+      height: 300,
+      data: salesByArea,
+      xField: 'sales',
+      yField: 'area',
+      yAxis: {
+        label: {
+          rotate: -Math.PI / 6,
+        },
+      },
+    });
+
+    bar.render();
+    const axisOptions = bar.chart.getOptions().axes;
+
+    // @ts-ignore
+    expect(axisOptions.area.label.rotate).toBe(-Math.PI / 6);
+  });
+});

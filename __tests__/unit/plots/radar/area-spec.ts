dd898f7f8ca74fc2d03059cffd719f45dff6bfea
kasmine <736929286@qq.com>
2020-07-26 18:20:39 +0800

    feat(v2/radar): add radar plot
    - [x] field
    - [x] meta
    - [x] axis
    - [x] geometry: line,point,area
    - [x] style: lineStyle, point style, area style
    - [x] shape: point shape
    - [ ] tooltip: 横向共享展示 tooltip
    - [ ] active style: 自定义hover active样式,取消 tooltip marker

---
+++ __tests__/unit/plots/radar/area-spec.ts
@@ -0,0 +1,83 @@
+import { Radar } from '../../../../src';
+import { SERIES_DATA } from '../../../data/radar';
+import { createDiv } from '../../../utils/dom';
+
+describe('radar with area', () => {
+  it('add area geometry', () => {
+    const radar = new Radar(createDiv(), {
+      width: 400,
+      height: 300,
+      data: SERIES_DATA,
+      xField: 'name',
+      yField: 'value',
+      seriesField: 'type',
+      radius: 0.8,
+      color: ['red', 'orange'],
+      area: {},
+    });
+
+    radar.render();
+    expect(radar.chart.geometries.length).toBe(2);
+
+    const areaGeometry = radar.chart.geometries[1];
+    expect(areaGeometry.elements[0].getModel().color).toBe('red');
+    expect(areaGeometry.elements[1].getModel().color).toBe('orange');
+    expect(areaGeometry.type).toBe('area');
+    expect(areaGeometry.elements.length).toBe(2);
+  });
+
+  it('area style', () => {
+    const radar = new Radar(createDiv(), {
+      width: 400,
+      height: 300,
+      data: SERIES_DATA,
+      xField: 'name',
+      yField: 'value',
+      seriesField: 'type',
+      radius: 0.8,
+      color: ['red', 'orange'],
+      area: {
+        style: {
+          fill: 'rgba(0, 0, 0, 0.10)',
+          fillOpacity: 0.3,
+        },
+      },
+    });
+
+    radar.render();
+
+    const areaGeometry = radar.chart.geometries[1];
+    expect(areaGeometry.elements[0].getModel().color).toBe('red');
+    expect(areaGeometry.elements[1].getModel().color).toBe('orange');
+    expect(areaGeometry.elements[0].getModel().style.fill).toBe('rgba(0, 0, 0, 0.10)');
+    expect(areaGeometry.elements[0].getModel().style.fillOpacity).toBe(0.3);
+  });
+
+  it('area style, with callback', () => {
+    const radar = new Radar(createDiv(), {
+      width: 400,
+      height: 300,
+      data: SERIES_DATA,
+      xField: 'name',
+      yField: 'value',
+      seriesField: 'type',
+      radius: 0.8,
+      color: ['red', 'orange'],
+      area: {
+        style: (x, y, series) => {
+          return {
+            fill: 'rgb(0, 0, 0)',
+            fillOpacity: series === '实际支出' ? 0.1 : 0.3,
+          };
+        },
+      },
+    });
+
+    radar.render();
+
+    const areaGeometry = radar.chart.geometries[1];
+    expect(areaGeometry.elements[0].getModel().style.fill).toBe('rgb(0, 0, 0)');
+    expect(areaGeometry.elements[0].getModel().style.fillOpacity).toBe(0.3);
+    expect(areaGeometry.elements[1].getModel().style.fillOpacity).toBe(0.1);
+  });
+});

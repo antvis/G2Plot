ad2684b4fe7da685ad7b12eb951dc1c367cefa23
connono <36756846+connono@users.noreply.github.com>
2020-07-27 21:05:45 +0800

    V2 tiny tooltip (#1345)
    * feat(minichart): add tooltip
    
    * fix: stash代码合并错误
    
    * fix: 代码复用
    
    * fix: 增加注释,代码复用
    
    Co-authored-by: yinshi.wyl <yinshi.wyl@antfin.com>

--- src/types/tooltip.ts
+++ src/types/tooltip.ts
@@ -1,3 +1,16 @@
 import { TooltipOption } from '@antv/g2/lib/interface';
 
 export type Tooltip = TooltipOption;
+
+export type TinyTooltipOption = {
+  /** tootip body模版语言 */
+  readonly formatter?: (x: number, y: number) => string;
+  /** 获取tooltip内部dom节点覆写样式 */
+  readonly domStyles?: object;
+  /** tooltip定位位置 */
+  readonly position?: 'top' | 'bottom' | 'left' | 'right';
+  /** tooltip偏移位置 */
+  readonly offset?: number;
+  /** 是否显示交叉线 */
+  readonly showCrosshairs?: boolean;
+};

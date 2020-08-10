b4d2144d9b02537bdc040cfa06f24192cc2f5ba2
hustcc <i@hust.cc>
2020-08-10 19:41:29 +0800

    fix: test cases

--- src/core/plot.ts
+++ src/core/plot.ts
@@ -3,7 +3,7 @@ import { deepMix } from '@antv/util';
 import EE from '@antv/event-emitter';
 import { bind } from 'size-sensor';
 import { Adaptor } from './adaptor';
-import { ChartOptions, Data } from '../types';
+import { ChartOptions, Data, Size } from '../types';
 import { getContainerSize } from '../utils';
 
 /**
@@ -37,19 +37,31 @@ export abstract class Plot<O extends ChartOptions> extends EE {
    */
   private createG2() {
     const { width, height, padding, appendPadding, renderer, pixelRatio, autoFit = true } = this.options;
-    // autoFit 开启的时候，计算默认的大小
-    const size = autoFit ? getContainerSize(this.container) : { width, height };
 
     this.chart = new Chart({
       container: this.container,
       autoFit: false, // G2Plot 使用 size-sensor 进行 autoFit
-      ...size,
+      ...this.getChartSize(width, height, autoFit),
       padding,
       appendPadding,
       renderer,
       pixelRatio,
       localRefresh: false, // 默认关闭，目前 G 还有一些位置问题，难以排查！
     });
+  }
+
+  /**
+   * 计算默认的 chart 大小。逻辑简化：如果存在 width height，则直接使用，否则使用容器大小
+   * @param width
+   * @param height
+   * @param autoFit
+   */
+  private getChartSize(width: number, height: number, autoFit: boolean): Size {
+    if (width && height) {
+      return { width, height };
+    }
+
+    return { width, height, ...getContainerSize(this.container) };
   }
 
   /**

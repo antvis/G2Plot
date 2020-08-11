8d12f877788eda2e25832d6a23a49affde4d0726
hustcc <i@hust.cc>
2020-08-11 10:08:52 +0800

    feat(area): add area demo

---
+++ examples/area/stacked/demo/area-large-data.ts
@@ -0,0 +1,25 @@
+import { Area } from '@antv/g2plot';
+
+fetch('https://gw.alipayobjects.com/os/bmw-prod/55424a73-7cb8-4f79-b60d-3ab627ac5698.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const area = new Area('container', {
+      data,
+      xField: 'year',
+      yField: 'value',
+      seriesField: 'category',
+      color: ['#6897a7', '#8bc0d6', '#60d7a7', '#dedede', '#fedca9', '#fab36f', '#d96d6f'],
+      xAxis: {
+        type: 'time',
+        mask: 'YYYY',
+      },
+      yAxis: {
+        label: {
+          // 数值格式化为千分位
+          formatter: (v) => `${v}`.replace(/\d{1,3}(?=(\d{3})+$)/g, (s) => `${s},`),
+        },
+      },
+    });
+
+    area.render();
+  });

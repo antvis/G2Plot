dd7b26d16618ee3786c2ed7ed03e97d53b20bc5a
hustcc <i@hust.cc>
2020-08-18 19:39:00 +0800

    Chore walkthrough (#1455)
    * chore: remove defaultOption props
    
    * docs: add type defined

--- src/utils/g-util.ts
+++ src/utils/g-util.ts
@@ -1,7 +1,12 @@
 import { IGroup, IShape } from '@antv/g-base';
 import { Util } from '@antv/g2';
 
-export function groupTransform(group: IGroup | IShape, actions) {
+/**
+ * 对元素设置一些操作（旋转、移动等），每次重置 matrix
+ * @param group
+ * @param actions
+ */
+export function groupTransform(group: IGroup | IShape, actions: any[][]) {
   const ulMatrix = [1, 0, 0, 0, 1, 0, 0, 0, 1];
   const matrix = Util.transform(ulMatrix, actions);
   group.setMatrix(matrix);

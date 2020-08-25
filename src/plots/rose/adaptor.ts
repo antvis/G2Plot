c40dc91edb6a8b0e2730cf32af658212fe9f73cd
BeiYuShuiGuoLvDeKongQi <958414905@qq.com>
2020-08-25 23:34:55 +0800

    chore:  优化代码

--- src/plots/rose/adaptor.ts
+++ src/plots/rose/adaptor.ts
@@ -50,8 +50,8 @@ function label(params: Params<RoseOptions>): Params<RoseOptions> {
       cfg,
     });
   } else {
-    // label != null 表示 label 非 undefined 且非 null
-    label != null && log(LEVEL.WARN, false, 'the label option must be an Object.');
+    // label == null 表示 label 可能是 undefined 或者 null
+    log(LEVEL.WARN, label == null, 'the label option must be an Object.');
     geometry.label({ fields: [xField] });
   }
 

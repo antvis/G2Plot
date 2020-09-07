8d56005cb4ba9899a4bf96de43779bfe5fccbd16
arcsin1 <zy19931129@gmail.com>
2020-09-07 10:14:18 +0800

    feat: 新增v2bullet子弹图

---
+++ __tests__/data/bullet.ts
@@ -0,0 +1,3 @@
+export const bulletData = [{ title: '数学', ranges: [30, 50, 100], measures: [20], target: 85 }];
+
+export const bulletDatas = [{ title: '数学', ranges: [30, 50, 100], measures: [30, 30], target: 85 }];
---
+++ src/plots/bullet/index.ts
@@ -0,0 +1,44 @@
+import { deepMix } from '@antv/util';
+import { Plot } from '../../core/plot';
+import { Adaptor } from '../../core/adaptor';
+import { BulletOptions } from './types';
+import { adaptor } from './adaptor';
+
+export { BulletOptions };
+
+export class Bullet extends Plot<BulletOptions> {
+  /** 图表类型 */
+  public type: string = 'bullet';
+
+  /**
+   * 获取子弹图的适配器
+   */
+  protected getSchemaAdaptor(): Adaptor<BulletOptions> {
+    return adaptor;
+  }
+
+  protected getDefaultOptions() {
+    return deepMix({}, super.getDefaultOptions(), {
+      bulletLabel: {
+        position: 'right',
+      },
+      legend: false,
+      bulletStyle: {
+        target: {
+          size: 20,
+          color: '#223273',
+          style: {
+            lineWidth: 2,
+          },
+        },
+        range: {
+          size: 30,
+          style: { fillOpacity: 0.65 },
+        },
+        measure: {
+          size: 20,
+        },
+      },
+    });
+  }
+}

8834fb5389e0acc54c7e678d69ae79ddc2159810
hustcc <i@hust.cc>
2020-09-07 19:34:54 +0800

    chore: area 走查优化 (#1541)
    * fix(area): #1536 修复面积图中 range 不为 0 1 的问题
    
    * feat(area): 添加 gradients 面积图 demo
    
    * feat(area): 添加 slider 的 demo

---
+++ examples/area/basic/demo/basic-gradients.ts
@@ -0,0 +1,21 @@
+import { Area } from '@antv/g2plot';
+
+fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const area = new Area('container', {
+      data,
+      xField: 'Date',
+      yField: 'scales',
+      xAxis: {
+        type: 'dateTime',
+        tickCount: 5,
+      },
+      areaStyle: () => {
+        return {
+          fill: 'l(270) 0:#ffffff 0.5:#7ec2f3 1:#1890ff',
+        };
+      },
+    });
+    area.render();
+  });

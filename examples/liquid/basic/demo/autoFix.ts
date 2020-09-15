6bb0e8588179b3d2c01d842f57a79632a243c2f1
罗漪 <845201776@qq.com>
2020-09-15 11:56:06 +0800

    update liquid-demo

--- examples/liquid/basic/demo/autoFix.ts
+++ examples/liquid/basic/demo/autoFix.ts
@@ -5,16 +5,16 @@ const liquidPlot = new Liquid(document.getElementById('container'), {
   height: 300,
   autoFit: false,
   percent: 0.75,
+  statistic: {
+    formatter: (v) => {
+      return `占比${v * 100}%`;
+    },
+  },
   liquidStyle: (v) => {
     return {
       fill: v > 0.75 ? 'red' : '#3b78e775',
     };
   },
   color: () => '#3b78e775',
-  statistic: {
-    formatter: (v) => {
-      return `占比${v * 100}%`;
-    },
-  },
 });
 liquidPlot.render();
--- examples/liquid/basic/demo/basic.ts
+++ examples/liquid/basic/demo/basic.ts
@@ -1,9 +1,6 @@
 import { Liquid } from '@antv/g2plot';
 
 const liquidPlot = new Liquid(document.getElementById('container'), {
-  width: 600,
-  height: 300,
-  autoFit: false,
   percent: 0.75,
 });
 liquidPlot.render();

666426f1f13914f69b926b2bf886da8716ef1867
被雨水过滤的空气 <958414905@qq.com>
2020-09-21 19:02:32 +0800

    feat(word-cloud): 主要是添加图表尺寸变化时自动重渲染 (#1603)
    * feat: add state
    
    * chore: 优化类型信息
    
    * fix: 尺寸变化时，重新渲染词云图
    
    * chore: 删除没有用到的配置项
    
    * docs: 基础示例autoFit设为true，添加交互效果的示例
    
    * fix: 解决容器元素销毁时代码陷入死循环的问题
    
    * chore: 优化注释

--- src/plots/word-cloud/index.ts
+++ src/plots/word-cloud/index.ts
@@ -18,6 +18,7 @@ export class WordCloud extends Plot<WordCloudOptions> {
   protected getDefaultOptions(): Partial<WordCloudOptions> {
     return deepMix({}, super.getDefaultOptions(), {
       timeInterval: 2000,
+      autoFit: true,
       tooltip: {
         showTitle: false,
         showMarkers: false,
@@ -46,7 +47,8 @@ export class WordCloud extends Plot<WordCloudOptions> {
    * 覆写父类的方法，因为词云图使用 data-set 进行布局，原理上有些不一样
    */
   protected triggerResize() {
-    // 重新做一遍 data-set 的处理逻辑，这个适和其他图形不一样的地阿芳
+    // 当整个词云图图表的宽高信息发生变化时，每个词语的坐标需要重新
+    // 交给 data-set 进行计算，不然会出现布局错乱，如相邻词语重叠的情况。
     this.execAdaptor();
     // 执行父类的方法
     super.triggerResize();

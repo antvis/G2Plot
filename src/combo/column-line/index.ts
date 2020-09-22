25235edb440caa0bcb4de2a8e76a3c63beb55f3a
liufu.lf <liufu.lf@antfin.com>
2020-09-22 11:01:28 +0800

    fix: 修复双轴图相关 bugs

--- src/combo/column-line/index.ts
+++ src/combo/column-line/index.ts
@@ -2,7 +2,10 @@ import { deepMix } from '@antv/util';
 import BasePlot, { PlotConfig } from '../../base/plot';
 import ColumnLineLayer, { ColumnLineViewConfig } from './layer';
 
-export interface ColumnLineConfig extends ColumnLineViewConfig, PlotConfig {}
+export interface ColumnLineConfig extends ColumnLineViewConfig, PlotConfig {
+  /** 用户自定义 meta */
+  customMeta?: boolean;
+}
 
 export default class ColumnLine extends BasePlot<ColumnLineConfig> {
   public static getDefaultOptions: typeof ColumnLineLayer.getDefaultOptions = ColumnLineLayer.getDefaultOptions;

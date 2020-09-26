1a9f6da6b252d45a6bd2b032d0e407d8cb1eb21e
arcsin1 <zy19931129@gmail.com>
2020-09-26 13:52:04 +0800

    docs: 增加一个annatation的demo (#1638)

---
+++ examples/line/basic/demo/line-annotation-color.ts
@@ -0,0 +1,41 @@
+import { Line } from '@antv/g2plot';
+
+fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const line = new Line('container', {
+      data,
+      padding: 'auto',
+      xField: 'Date',
+      yField: 'scales',
+      annotations: [
+        // 低于中位数颜色变化
+        {
+          type: 'regionFilter',
+          start: ['min', 'median'],
+          end: ['max', '0'],
+          color: 'red',
+        },
+        {
+          type: 'text',
+          position: ['min', 'median'],
+          content: '中位数',
+          offsetY: -4,
+          style: {
+            textBaseline: 'bottom',
+          },
+        },
+        {
+          type: 'line',
+          start: ['min', 'median'],
+          end: ['max', 'median'],
+          style: {
+            stroke: 'red',
+            lineDash: [2, 2],
+          },
+        },
+      ],
+    });
+
+    line.render();
+  });

9013a55b161b018f43a5884af853739abab8cf56
hustcc <i@hust.cc>
2020-10-21 15:00:36 +0800

    fix(1758): 面积图 line.color 设置不生效的问题

---
+++ __tests__/bugs/issue-1758-spec.ts
@@ -0,0 +1,21 @@
+import { TinyArea } from '../../src';
+import { createDiv } from '../utils/dom';
+
+describe('#1758', () => {
+  it('area line.color', () => {
+    const tinyArea = new TinyArea(createDiv('mini 面积图 color 配置不生效'), {
+      height: 60,
+      width: 300,
+      autoFit: false,
+      data: new Array(100).fill(0).map(() => Math.random() * 100),
+      smooth: true,
+      line: {
+        color: 'red',
+      },
+    });
+
+    tinyArea.render();
+
+    expect(tinyArea.chart.geometries[1].elements[0].container.getChildByIndex(0).attr('stroke')).toBe('red');
+  });
+});

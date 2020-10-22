f4dedc31b6b896297238252125db65f1abe163d4
zhangzhonghe <958414905@qq.com>
2020-10-22 12:07:58 +0800

    feat(word-cloud): add colorField option

---
+++ examples/word-cloud/basic/demo/color-field.ts
@@ -0,0 +1,21 @@
+import { WordCloud } from '@antv/g2plot';
+
+fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/antv-keywords.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const wordCloud = new WordCloud('container', {
+      data,
+      width: 600,
+      height: 400,
+      wordField: 'name',
+      weightField: 'value',
+      colorField: 'value',
+      imageMask: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*07tdTIOmvlYAAAAAAAAAAABkARQnAQ',
+      wordStyle: {
+        fontFamily: 'Verdana',
+        fontSize: [8, 32],
+      },
+    });
+
+    wordCloud.render();
+  });

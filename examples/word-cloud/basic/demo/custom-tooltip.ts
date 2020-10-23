76cdeaca7c17ce2276256e3faf59f25e17748127
zhangzhonghe <958414905@qq.com>
2020-10-23 20:28:25 +0800

    fix(word-cloud): 修复tooltip小圆点的颜色不显示

--- examples/word-cloud/basic/demo/custom-tooltip.ts
+++ examples/word-cloud/basic/demo/custom-tooltip.ts
@@ -19,7 +19,7 @@ fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/antv-keywords.json')
           if (!data.length) return;
           return `<h4 style="margin-top: 1em;">title</h4>
             <li class="g2-tooltip-list-item" style="margin-bottom:4px;display:flex;align-items:center;">
-            <span style="background-color:${data[0]?.mappingData?.color};" class="g2-tooltip-marker"></span>
+            <span style="background-color:${data[0]?.color};" class="g2-tooltip-marker"></span>
             <span style="display:inline-flex;flex:1;justify-content:space-between">
               <span style="margin-right: 16px;">${data[0]?.data.text}:</span><span>${data[0]?.data.value}</span>
             </span>

cc69ff0a49186570dd467abc27ece4a50a3d6d14
hustcc <i@hust.cc>
2020-10-23 09:39:50 +0800

    feat: add lab domain (#1765)
    * feat: add lab domain
    
    * feat(lab): add one case

---
+++ src/lab.ts
@@ -0,0 +1,35 @@
+import { RadialBar } from './plots/radial-bar';
+
+/** 实验室图表所处的阶段 */
+export enum Stage {
+  DEV = 'DEV',
+  BETA = 'BETA',
+  STABLE = 'STABLE',
+}
+
+/**
+ * 不同阶段打印一些消息给开发者
+ * @param stage
+ */
+export function notice(stage: Stage, plotType: string) {
+  console.warn(
+    stage === Stage.DEV
+      ? `Plot '${plotType}' is in DEV stage, just give us issues.`
+      : stage === Stage.BETA
+      ? `Plot '${plotType}' is in BETA stage, DO NOT use it in production env.`
+      : stage === Stage.STABLE
+      ? `Plot '${plotType}' is in STABLE stage, import it by "import { ${plotType} } from '@antv/g2'".`
+      : 'invalid Stage type.'
+  );
+}
+
+/**
+ * 实验室图表，实验室中的图表分成不同的阶段。
+ */
+export class Lab {
+  /** 玉珏图 */
+  static get RadialBar() {
+    notice(Stage.DEV, 'RadialBar');
+    return RadialBar;
+  }
+}

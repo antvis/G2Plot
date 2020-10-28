2d1a95951ebd54d5d5268af68efbbf42796b2dfe
yujs <1062888640@qq.com>
2020-10-28 23:45:51 +0800

    feat: add some unit test files

--- __tests__/unit/plots/radial-bar/index-spec.ts
+++ __tests__/unit/plots/radial-bar/index-spec.ts
@@ -16,6 +16,18 @@ describe('radial-bar', () => {
         { question: '问题 6', percent: 0.84 },
         { question: '问题 7', percent: 1.0 },
         { question: '问题 8', percent: 1.2 },
+        { question: '问题 9', percent: 0.49 },
+        { question: '问题 10', percent: 0.52 },
+        { question: '问题 11', percent: 0.53 },
+        { question: '问题 12', percent: 0.84 },
+        { question: '问题 13', percent: 1.0 },
+        { question: '问题 14', percent: 1.2 },
+        { question: '问题 15', percent: 0.49 },
+        { question: '问题 16', percent: 0.52 },
+        { question: '问题 17', percent: 0.53 },
+        { question: '问题 18', percent: 0.84 },
+        { question: '问题 19', percent: 1.0 },
+        { question: '问题 20', percent: 1.2 },
       ],
       xField: 'question',
       yField: 'percent',
@@ -25,6 +37,18 @@ describe('radial-bar', () => {
         cursor: 'pointer',
       },
       color: '#BAE7FF-#1890FF-#0050B3',
+      tooltip: {
+        showTitle: false,
+        showCrosshairs: false,
+      },
+      xAxis: {
+        line: {
+          style: {
+            lineWidth: 1,
+            fill: 'red',
+          },
+        },
+      },
     });
     radialBar.render();
   });

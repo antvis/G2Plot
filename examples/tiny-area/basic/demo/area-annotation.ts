a128a8afd67ceca4f8bab21e93eaa76875bf6900
lixi <1491812683@qq.com>
2020-10-30 15:30:43 +0800

    feat: optimize mini line and area demos

---
+++ examples/tiny-area/basic/demo/area-annotation.ts
@@ -0,0 +1,80 @@
+import { TinyArea } from '@antv/g2plot';
+
+const data = [
+  264,
+  417,
+  438,
+  887,
+  309,
+  397,
+  550,
+  575,
+  563,
+  430,
+  525,
+  592,
+  492,
+  467,
+  513,
+  546,
+  983,
+  340,
+  539,
+  243,
+  226,
+  192,
+];
+
+const avg = data.reduce((pre, cur) => pre + cur, 0) / data.length;
+const targetVal = 800;
+
+const tinyArea = new TinyArea('container', {
+  height: 60,
+  width: 300,
+  autoFit: false,
+  data,
+  smooth: true,
+  tooltip: false,
+  annotations: [
+    // 平均值
+    {
+      type: 'line',
+      start: ['min', avg],
+      end: ['max', avg],
+      text: {
+        content: '平均值',
+        offsetY: -2,
+        style: {
+          textAlign: 'left',
+          fontSize: 10,
+          fill: 'rgba(44, 53, 66, 0.45)',
+          textBaseline: 'bottom',
+        },
+      },
+      style: {
+        stroke: 'rgba(0, 0, 0, 0.25)',
+      },
+    },
+    // 目标值
+    {
+      type: 'line',
+      start: ['min', targetVal],
+      end: ['max', targetVal],
+      text: {
+        content: '目标值',
+        offsetY: -2,
+        style: {
+          textAlign: 'left',
+          fontSize: 10,
+          fill: 'rgba(44, 53, 66, 0.45)',
+          textBaseline: 'bottom',
+        },
+      },
+      style: {
+        stroke: 'rgba(0, 0, 0, 0.55)',
+      },
+    },
+  ],
+});
+
+tinyArea.render();

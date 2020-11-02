ad131291bcd269a1f2ec2abe8ad10efab2d8f09f
liufu.lf <liufu.lf@antfin.com>
2020-11-02 18:58:57 +0800

    fix: 新增单测

--- src/utils/deep-assign.ts
+++ src/utils/deep-assign.ts
@@ -6,7 +6,7 @@ const toString = {}.toString;
 const isType = (value: any, type: string): boolean => toString.call(value) === '[object ' + type + ']';
 
 const isArray = (value: any): value is Array<any> => {
-  return Array.isArray ? Array.isArray(value) : isType(value, 'Array');
+  return isType(value, 'Array');
 };
 
 const isObjectLike = (value: any): value is object => {
@@ -14,7 +14,6 @@ const isObjectLike = (value: any): value is object => {
    * isObjectLike({}) => true
    * isObjectLike([1, 2, 3]) => true
    * isObjectLike(Function) => false
-   * isObjectLike(null) => false
    */
   return typeof value === 'object' && value !== null;
 };
@@ -24,13 +23,9 @@ const isPlainObject = (value: any): value is object => {
    * isObjectLike(new Foo) => false
    * isObjectLike([1, 2, 3]) => false
    * isObjectLike({ x: 0, y: 0 }) => true
-   * isObjectLike(Object.create(null)) => true
    */
   if (!isObjectLike(value) || !isType(value, 'Object')) {
     return false;
-  }
-  if (Object.getPrototypeOf(value) === null) {
-    return true;
   }
   let proto = value;
   while (Object.getPrototypeOf(proto) !== null) {

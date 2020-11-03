fc4bfe75b59c131c4810d8ebb92481caf465e32f
hustcc <i@hust.cc>
2020-11-03 11:38:36 +0800

    fix: type define of legend, axis

--- src/types/axis.ts
+++ src/types/axis.ts
@@ -1,4 +1,4 @@
 import { AxisCfg } from '@antv/g2/lib/interface';
 import { Meta } from './meta';
 
-export type Axis = (AxisCfg & Omit<Meta, 'values' | 'formatter'>) | boolean;
+export type Axis = false | (AxisCfg & Omit<Meta, 'values' | 'formatter'>);
--- src/types/legend.ts
+++ src/types/legend.ts
@@ -1,3 +1,3 @@
 import { LegendCfg } from '@antv/g2/lib/interface';
 
-export type Legend = boolean | LegendCfg;
+export type Legend = false | LegendCfg;

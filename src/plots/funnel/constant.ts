5e833dc57d89edbe4f74633020de6ec426125e38
hustcc <i@hust.cc>
2020-11-18 09:21:52 +0800

    Update constant.ts

--- src/plots/funnel/constant.ts
+++ src/plots/funnel/constant.ts
@@ -4,7 +4,7 @@ export const FUNNEL_PERCENT = '$$percentage$$';
 export const FUNNEL_CONVERSATION = '$$conversion$$';
 // 漏斗单项占总体和的百分比，用于动态漏斗图计算高度：
 // data[n][yField] / sum(data[0-n][yField])
-export const FUNNEL_TOTAL_PERCENT = '$$total_percentage$$';
+export const FUNNEL_TOTAL_PERCENT = '$$totalPercentage$$';
 // 漏斗多边型 x 坐标
 export const PLOYGON_X = '$$x$$';
 export const PLOYGON_Y = '$$y$$';

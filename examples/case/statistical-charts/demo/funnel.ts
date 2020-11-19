8e4ae98ac543bf63b4368d488011534536a6f6ae
刘珍莹 <1803275951@qq.com>
2020-11-19 09:40:33 +0800

    Feat/funnel tooltip demo (#1943)
    * feat: 新增场景漏斗图，等待进一步设计稿
    
    * feat: 场景化漏斗 tooltip 示例
    
    * feat: 增加场景化 tooltip
    
    Co-authored-by: aiyin.lzy <nadia.lzy@antfin.com>

---
+++ examples/case/statistical-charts/demo/funnel.ts
@@ -0,0 +1,64 @@
+import { Funnel } from '@antv/g2plot';
+
+const originData = [
+  { stage: '触达次数', number: 789 },
+  { stage: '响应次数', number: 453 },
+  { stage: '分享次数', number: 193 },
+];
+
+const data = originData.map((item, index, arr) => {
+  if (index === 0) return item;
+  return {
+    ...item,
+    unConversation: arr[index - 1].number - item.number,
+    conversionPec: item.number / arr[index - 1].number,
+    unConversionPec: 1 - item.number / arr[index - 1].number,
+  };
+});
+
+const containerStyle = 'margin: 16px -8px; display: flex;';
+const boxStyle = 'padding: 0px 16px;';
+const titleStyle = 'font-weight: bold;';
+const tooltipItemStyle = 'margin-top: 12px; display: flex; width: 120px; justify-content: space-between;';
+
+const funnelPlot = new Funnel('container', {
+  data: data,
+  xField: 'stage',
+  yField: 'number',
+  legend: false,
+  conversionTag: false,
+  tooltip: {
+    follow: true,
+    enterable: true,
+    offset: 5,
+    customContent: (value, items) => {
+      if (!items || items.length <= 0) return;
+      const { data: itemData } = items[0];
+
+      if (itemData.stage === '触达次数') {
+        return `<div style='${tooltipItemStyle} margin-bottom: 12px; padding: 0 8px;'><span>待转化人数</span><span>${itemData.number}</span></div>`;
+      }
+
+      return (
+        `<div style='${containerStyle}'>` +
+        `<div style='${boxStyle} border-right: 1px solid #c2c2c2'>` +
+        `<div style='${titleStyle}'>转化</div>` +
+        `<div style='${tooltipItemStyle}'><span>转化人数</span><span>${itemData.number}</span></div>` +
+        `<div style='${tooltipItemStyle}'><span>转化率</span><span>${(itemData.conversionPec * 100).toFixed(
+          0
+        )}%</span></div>` +
+        `</div>` +
+        `<div style='${boxStyle}'>` +
+        `<div style='${titleStyle}'>未转化</div>` +
+        `<div style='${tooltipItemStyle}'><span>未转化人数</span><span>${itemData.unConversation}</span></div>` +
+        `<div style='${tooltipItemStyle}'><span>未转化率</span><span>${(itemData.unConversionPec * 100).toFixed(
+          0
+        )}%</span></div>` +
+        `</div>` +
+        `</div>`
+      );
+    },
+  },
+});
+
+funnelPlot.render();

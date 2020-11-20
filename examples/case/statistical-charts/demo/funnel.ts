624522cdc5fd88df16265f1da457f74d27c83476
aiyin.lzy <nadia.lzy@antfin.com>
2020-11-20 16:40:23 +0800

    feat: 更新进度条图&环图缩略图

--- examples/case/statistical-charts/demo/funnel.ts
+++ examples/case/statistical-charts/demo/funnel.ts
@@ -1,4 +1,5 @@
 import { Funnel } from '@antv/g2plot';
+import { insertCss } from 'insert-css';
 
 const data = [
   { stage: '触达次数', times: 789, uv: 1000, conversionUV: 0 },
@@ -6,11 +7,34 @@ const data = [
   { stage: '分享次数', times: 193, uv: 600, conversionUV: 200 },
 ];
 
-const containerStyle = 'margin: 16px -8px; display: flex;';
-const boxStyle = 'padding: 0px 16px;';
-const titleStyle = 'font-weight: bold;';
-const tooltipItemStyle = 'margin-top: 12px; display: flex; width: 120px; justify-content: space-between;';
-const linkStyle = 'display: inline-block; margin-top: 12px; color: #5B8FF9; text-decoration: none;';
+insertCss(`
+  .container{
+    margin: 16px -8px; display: flex;
+  }
+  .box{
+    padding: 0px 16px;
+  }
+  .title{
+    font-weight: bold;
+  }
+  .tooltip-item{
+    margin-top: 12px;
+    display: flex;
+    width: 120px;
+    justify-content: space-between;
+  }
+  .link{
+    display: inline-block;
+    margin-top: 12px;
+    color: #5B8FF9;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  .link:hover{
+
+    color: #5D7092;
+  }
+`);
 
 const funnelPlot = new Funnel('container', {
   data: data,
@@ -32,26 +56,23 @@ const funnelPlot = new Funnel('container', {
       const { data: itemData } = items[0];
 
       return (
-        `<div style='${containerStyle}'>` +
-        `<div style='${boxStyle} border-right: 1px solid #c2c2c2'>` +
-        `<div style='${titleStyle}'>转化</div>` +
-        `<div style='${tooltipItemStyle}'><span>转化人数</span><span>${itemData.conversionUV}</span></div>` +
-        `<div style='${tooltipItemStyle}'><span>转化率</span><span>${(
-          (itemData.conversionUV / itemData.uv) *
-          100
-        ).toFixed(0)}%</span></div>` +
-        `<a href="https://g2plot.antv.vision/zh" target='_blank' style='${linkStyle}'>查看转化详情</a>` +
+        `<div class='container'>` +
+        `<div class='box' style='border-right: 1px solid #c2c2c2'>` +
+        `<div class='title'>转化</div>` +
+        `<div class='tooltip-item'><span>转化人数</span><span>${itemData.conversionUV}</span></div>` +
+        `<div class='tooltip-item'><span>转化率</span><span>${((itemData.conversionUV / itemData.uv) * 100).toFixed(
+          0
+        )}%</span></div>` +
+        `<a class='link'>查看转化详情</a>` +
         `</div>` +
-        `<div style='${boxStyle}'>` +
-        `<div style='${titleStyle}'>未转化</div>` +
-        `<div style='${tooltipItemStyle}'><span>未转化人数</span><span>${
-          itemData.uv - itemData.conversionUV
-        }</span></div>` +
-        `<div style='${tooltipItemStyle}'><span>未转化率</span><span>${(
+        `<div class='box'>` +
+        `<div class='title'>未转化</div>` +
+        `<div class='tooltip-item'><span>未转化人数</span><span>${itemData.uv - itemData.conversionUV}</span></div>` +
+        `<div class='tooltip-item'><span>未转化率</span><span>${(
           (1 - itemData.conversionUV / itemData.uv) *
           100
         ).toFixed(0)}%</span></div>` +
-        `<a href="https://g2plot.antv.vision/zh" target='_blank' style='${linkStyle}'>查看未转化详情</a>` +
+        `<a class='link'>查看未转化详情</a>` +
         `</div>` +
         `</div>`
       );

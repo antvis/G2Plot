00fcef3e2f13700afe02f53b1734c5a43acc143a
lxfu1 <954055752@qq.com>
2020-11-21 11:01:24 +0800

    fix: 修复热力图颜色映射错误

--- src/plots/heatmap/shapes/square.ts
+++ src/plots/heatmap/shapes/square.ts
@@ -16,16 +16,16 @@ registerShape('polygon', 'square', {
     const sizeRatio = Number(cfg.shape[2]);
     const lenRatio = Math.sqrt(sizeRatio);
     const sideLength = maxSideLength * lenRatio * Math.sqrt(value);
-
+    const fill = cfg.style?.fill || cfg.color || cfg.defaultStyle?.fill;
     const polygon = group.addShape('rect', {
       attrs: {
         x: cx - sideLength / 2,
         y: cy - sideLength / 2,
         width: sideLength,
         height: sideLength,
-        fill: cfg.color,
         ...cfg.defaultStyle,
         ...cfg.style,
+        fill,
       },
     });
     return polygon;

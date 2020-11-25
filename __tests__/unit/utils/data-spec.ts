da9c345fd1a05cd62227a3535fe413785070ccc7
hustcc <i@hust.cc>
2020-11-25 14:10:57 +0800

    fix(y-scale): update y scale min, max

--- __tests__/unit/utils/data-spec.ts
+++ __tests__/unit/utils/data-spec.ts
@@ -2,8 +2,16 @@ import { adjustYMetaByZero } from '../../../src/utils/data';
 
 describe('data', () => {
   it('adjustYMetaByZero', () => {
-    expect(adjustYMetaByZero([{ y: 0 }, { y: 1 }], 'y')).toEqual({});
+    expect(adjustYMetaByZero([{ y: -1 }, { y: 1 }], 'y')).toEqual({});
+
+    expect(adjustYMetaByZero([{ y: 0 }, { y: 1 }], 'y')).toEqual({ min: 0 });
     expect(adjustYMetaByZero([{ y: 20 }, { y: 20 }], 'y')).toEqual({ min: 0 });
     expect(adjustYMetaByZero([{ y: -20 }, { y: -20 }], 'y')).toEqual({ max: 0 });
+
+    expect(adjustYMetaByZero([{ y: 20 }, { y: 20 }, { y: null }], 'y')).toEqual({ min: 0 });
+    expect(adjustYMetaByZero([{ y: -20 }, { y: -20 }, { y: 0 }], 'y')).toEqual({ max: 0 });
+    expect(adjustYMetaByZero([{ y: -20 }, { y: -20 }, { y: undefined }], 'y')).toEqual({ max: 0 });
+
+    expect(adjustYMetaByZero([{ y: null }, { y: 0 }, { y: undefined }], 'y')).toEqual({ min: 0 });
   });
 });
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,4 +1,4 @@
-import { get } from '@antv/util';
+import { get, isNil } from '@antv/util';
 import { Data, Datum, Meta } from '../types';
 
 /**
@@ -7,16 +7,15 @@ import { Data, Datum, Meta } from '../types';
  * @param field
  */
 export function adjustYMetaByZero(data: Data, field: string): Meta {
-  const gtZero = data.every((datum: Datum) => get(datum, [field]) > 0);
-  const ltZero = data.every((datum: Datum) => get(datum, [field]) < 0);
+  const gtZero = data.every((datum: Datum) => isNil(get(datum, [field])) || get(datum, [field]) >= 0);
+  const ltZero = data.every((datum: Datum) => isNil(get(datum, [field])) || get(datum, [field]) <= 0);
 
   // 目前是增量更新，对 { min: 0, max: undefined } 进行 update({ max: 0 }) 会得到 { min: 0, max: 0 }
-  // 需要添加 undefined 进行覆盖
   if (gtZero) {
-    return { min: 0, max: undefined };
+    return { min: 0 };
   }
   if (ltZero) {
-    return { max: 0, min: undefined };
+    return { max: 0 };
   }
   return {};
 }

cc2edaf860befdc4d0d9e61a7c8e5937304b3bd3
hustcc <i@hust.cc>
2020-12-02 17:31:12 +0800

    feat:  new plot sankey 桑基图 (#2016)
    * feat: init sankey
    
    * feat(sankey): add sankey layout
    
    * test(sankey): add test for sankey transform
    
    * feat(sankey): draw sankey by transform data & arc shape
    
    * feat(sankey): adaptor & test case
    
    * feat(sankey): demo & docs
    
    * test(sankey): add test for data process
    
    * chore: remove unused file & import

---
+++ src/plots/sankey/index.ts
@@ -0,0 +1,66 @@
+import { Plot } from '../../core/plot';
+import { Adaptor } from '../../core/adaptor';
+import { Datum } from '../../types';
+import { SankeyOptions } from './types';
+import { adaptor } from './adaptor';
+
+export { SankeyOptions };
+
+/**
+ *  桑基图 Sankey
+ */
+export class Sankey extends Plot<SankeyOptions> {
+  /** 图表类型 */
+  public type: string = 'sankey';
+
+  protected getDefaultOptions() {
+    return {
+      appendPadding: 8,
+      syncViewPadding: true,
+      nodeStyle: {
+        opacity: 1,
+        fillOpacity: 1,
+        lineWidth: 1,
+      },
+      edgeStyle: {
+        opacity: 0.3,
+        lineWidth: 0,
+      },
+      label: {
+        fields: ['x', 'name'],
+        callback: (x: number[], name: string) => {
+          const isLast = x[1] === 1; // 最后一列靠边的节点
+          return {
+            style: {
+              fill: '#545454',
+              textAlign: isLast ? 'end' : 'start',
+            },
+            offsetX: isLast ? -8 : 8,
+            content: name,
+          };
+        },
+      },
+      tooltip: {
+        showTitle: false,
+        showMarkers: false,
+        fields: ['source', 'target', 'value'],
+        formatter: (datum: Datum) => {
+          const { source, target, value } = datum;
+          return {
+            name: source + ' -> ' + target,
+            value,
+          };
+        },
+      },
+      nodeWidthRatio: 0.008,
+      nodePaddingRatio: 0.01,
+    };
+  }
+
+  /**
+   * 获取适配器
+   */
+  protected getSchemaAdaptor(): Adaptor<SankeyOptions> {
+    return adaptor;
+  }
+}

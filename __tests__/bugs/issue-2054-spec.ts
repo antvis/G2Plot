e6b0f0a285d24a3be112190b39d69c659a662a7c
kasmine <736929286@qq.com>
2020-12-02 17:57:45 +0800

    fix: 修复测试用例

--- __tests__/bugs/issue-2054-spec.ts
+++ __tests__/bugs/issue-2054-spec.ts
@@ -5,15 +5,17 @@ describe('#2054', () => {
   it('pie {percentage} label', () => {
     const pie = new Pie(createDiv(), {
       width: 400,
-      height: 300,
-      data: [
-        { type: '1', value: 10 },
-        { type: '2', value: 40 },
-        { type: '3', value: 0 },
-        { type: '4', value: null },
-      ],
+      height: 400,
+      radius: 1,
+      innerRadius: 0.3,
       angleField: 'value',
       colorField: 'type',
+      data: [
+        { type: 'type-1', value: 10 },
+        { type: 'type-2', value: 40 },
+        { type: 'type-3', value: 0 },
+        { type: 'type-4', value: null },
+      ],
       label: {
         content: '{percentage}',
       },
@@ -22,10 +24,8 @@ describe('#2054', () => {
     pie.render();
 
     const labels = pie.chart.geometries[0].labelsContainer.getChildren();
-    // @ts-ignore
-    expect(labels[2].find((shape) => shape.get('type') === 'text')).toEqual('0.00%');
-    // @ts-ignore
-    expect(labels[3].find((shape) => shape.get('type') === 'text')).toEqual(null);
+    expect(labels[2].get('children')[0].attr('text')).toBe('0.00%');
+    expect(labels[3].get('children')[0].attr('text')).toBe(null);
 
     pie.destroy();
   });

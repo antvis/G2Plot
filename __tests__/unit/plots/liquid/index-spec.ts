3c72a85c1a902e7419235a640f39a6c001ebaf5b
hustcc <i@hust.cc>
2020-12-11 13:24:35 +0800

    feat(liquid): add outline, wave options for customize

--- __tests__/unit/plots/liquid/index-spec.ts
+++ __tests__/unit/plots/liquid/index-spec.ts
@@ -7,7 +7,7 @@ describe('liquid', () => {
       width: 600,
       height: 300,
       autoFit: false,
-      percent: 0.75,
+      percent: 0.45,
     });
 
     liquid.render();
@@ -16,16 +16,65 @@ describe('liquid', () => {
     expect(liquid.options.radius).toBe(0.9);
 
     // @ts-ignore
-    expect(liquid.chart.middleGroup.getChildren()[0].getChildren()[1].attr('r')).toBe(135);
+    expect(liquid.chart.middleGroup.getChildren()[0].getChildren()[0].attr('r')).toBe(135);
 
     // 宽 < 高，按照高度来设置 radius
     liquid.changeSize(300, 500);
-
     // @ts-ignore
-    expect(liquid.chart.middleGroup.getChildren()[0].getChildren()[1].attr('r')).toBe(135); // circle
+    expect(liquid.chart.middleGroup.getChildren()[0].getChildren()[0].attr('r')).toBe(135);
+    // 宽 < 高，按照高度来设置 radius
+    liquid.changeSize(500, 500);
+    // @ts-ignore
+    expect(liquid.chart.middleGroup.getChildren()[0].getChildren()[0].attr('r')).toBe(225);
 
     liquid.destroy();
   });
+
+  it('outline & wave', () => {
+    const liquid = new Liquid(createDiv(), {
+      width: 600,
+      height: 300,
+      autoFit: false,
+      percent: 0.3,
+    });
+
+    liquid.render();
+    expect(liquid.options.outline).toEqual({ border: 2, distance: 0 });
+    expect(liquid.options.wave).toEqual({ count: 3, length: 192 });
+
+    expect(liquid.chart.middleGroup.findAllByName('wrap')[0].attr('r')).toBe(135);
+    expect(liquid.chart.middleGroup.findAllByName('wrap')[0].attr('lineWidth')).toBe(2);
+    expect(liquid.chart.middleGroup.findAllByName('waterwave-path').length).toBe(3);
+    expect(liquid.chart.middleGroup.findAllByName('waterwave-path')[0].get('animations')[0].toAttrs.matrix[6]).toBe(
+      192
+    );
+    expect(liquid.chart.middleGroup.findAllByName('waves')[0].get('clipShape').attr('r')).toBe(135);
+
+    liquid.update({
+      outline: {
+        border: 4,
+        distance: 8,
+      },
+      wave: {
+        count: 5,
+        length: 128,
+      },
+    });
+
+    expect(liquid.options.outline).toEqual({ border: 4, distance: 8 });
+    expect(liquid.options.wave).toEqual({ count: 5, length: 128 });
+
+    expect(liquid.chart.middleGroup.findAllByName('wrap')[0].attr('r')).toBe(135);
+    expect(liquid.chart.middleGroup.findAllByName('wrap')[0].attr('lineWidth')).toBe(4);
+    expect(liquid.chart.middleGroup.findAllByName('waterwave-path').length).toBe(5);
+    expect(liquid.chart.middleGroup.findAllByName('waterwave-path')[0].get('animations')[0].toAttrs.matrix[6]).toBe(
+      128
+    );
+    expect(liquid.chart.middleGroup.findAllByName('waves')[0].get('clipShape').attr('r')).toBe(127);
+
+    liquid.destroy();
+  });
+
   it('change data', () => {
     const liquid = new Liquid(createDiv(), {
       width: 600,

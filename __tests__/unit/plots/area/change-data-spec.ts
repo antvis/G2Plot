6baf40491b5c8e71006ce13feefdbebfcb20fcea
kasmine <736929286@qq.com>
2020-12-29 07:50:35 +0800

    feat(area): 增加测试用例

---
+++ __tests__/unit/plots/area/change-data-spec.ts
@@ -0,0 +1,50 @@
+import { Area } from '../../../../src';
+import { partySupport } from '../../../data/party-support';
+import { createDiv } from '../../../utils/dom';
+
+describe('area', () => {
+  it('change data', () => {
+    const area = new Area(createDiv(), {
+      width: 400,
+      height: 300,
+      data: partySupport.filter((o) => ['FF'].includes(o.type)),
+      xField: 'date',
+      yField: 'value',
+      seriesField: 'type',
+    });
+
+    area.render();
+
+    expect(area.chart.geometries[0].elements.length).toBe(1);
+
+    area.changeData(partySupport.filter((o) => ['FF', 'Lab'].includes(o.type)));
+    expect(area.chart.geometries[0].elements.length).toBe(2);
+    expect(area.chart.geometries[1].elements.length).toBe(2);
+    expect(area.options.data).toEqual(partySupport.filter((o) => ['FF', 'Lab'].includes(o.type)));
+
+    area.destroy();
+  });
+
+  it('add point', () => {
+    const area = new Area(createDiv(), {
+      width: 400,
+      height: 300,
+      data: [
+        { type: '1', value: 10 },
+        { type: '2', value: 3 },
+      ],
+      xField: 'type',
+      yField: 'value',
+      line: null,
+      point: {},
+    });
+
+    area.render();
+    expect(area.chart.geometries[1].elements.length).toBe(2);
+
+    area.changeData([...area.options.data, { type: '3', value: 10 }]);
+    expect(area.chart.geometries[1].elements.length).toBe(3);
+
+    area.destroy();
+  });
+});

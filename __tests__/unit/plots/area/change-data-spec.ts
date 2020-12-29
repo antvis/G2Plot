3b8f4089e795a0d78ea9db387491cd9d5120b150
kasmine <736929286@qq.com>
2020-12-29 08:10:23 +0800

    feat(area): 增加百分比堆叠面积图的测试用例

--- __tests__/unit/plots/area/change-data-spec.ts
+++ __tests__/unit/plots/area/change-data-spec.ts
@@ -1,4 +1,5 @@
 import { Area } from '../../../../src';
+import { getAreaData } from '../../../../src/plots/area/utils';
 import { partySupport } from '../../../data/party-support';
 import { createDiv } from '../../../utils/dom';
 
@@ -44,6 +45,30 @@ describe('area', () => {
 
     area.changeData([...area.options.data, { type: '3', value: 10 }]);
     expect(area.chart.geometries[1].elements.length).toBe(3);
+
+    area.destroy();
+  });
+
+  it('stacked percent area', () => {
+    const area = new Area(createDiv(), {
+      width: 400,
+      height: 300,
+      data: partySupport.filter((o) => ['FF'].includes(o.type)),
+      xField: 'date',
+      yField: 'value',
+      seriesField: 'type',
+      isPercent: true,
+    });
+
+    area.render();
+
+    expect(area.chart.geometries[0].elements.length).toBe(1);
+
+    area.changeData(partySupport.filter((o) => ['FF', 'Lab'].includes(o.type)));
+    expect(area.chart.geometries[0].elements.length).toBe(2);
+    expect(area.chart.geometries[1].elements.length).toBe(2);
+    expect(area.options.data).toEqual(partySupport.filter((o) => ['FF', 'Lab'].includes(o.type)));
+    expect(area.chart.getData()).toEqual(getAreaData(area.options));
 
     area.destroy();
   });

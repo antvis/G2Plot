1aefd258c22255e39abbe039b81228f034e1cdf0
hustcc <i@hust.cc>
2020-12-30 16:12:44 +0800

    fix(conversion): conversion calculate error (#2160)
    * fix(conversion): conversion calculate error
    
    * fix: ci
    
    * fix: ci

--- __tests__/unit/utils/conversion-spec.ts
+++ __tests__/unit/utils/conversion-spec.ts
@@ -2,11 +2,12 @@ import { conversionTagFormatter } from '../../../src/utils/conversion';
 
 describe('conversionTagFormatter', () => {
   it('conversionTagFormatter', () => {
-    expect(conversionTagFormatter(0, 0)).toBe('0.00%');
+    expect(conversionTagFormatter(0, 0)).toBe('100%');
     expect(conversionTagFormatter(0, 1)).toBe('∞');
     expect(conversionTagFormatter(1, 0)).toBe('-∞');
     expect(conversionTagFormatter(10, 20)).toBe('200.00%');
     expect(conversionTagFormatter(40, 20)).toBe('50.00%');
+    expect(conversionTagFormatter(40, 40)).toBe('100%');
 
     expect(conversionTagFormatter(null, 20)).toBe('-');
     expect(conversionTagFormatter(20, null)).toBe('-');

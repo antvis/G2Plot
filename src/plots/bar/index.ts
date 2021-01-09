82178aea8e0d6136f8d44456cff3f59e86bd5311
kasmine <736929286@qq.com>
2021-01-10 00:26:31 +0800

    feat(bar): 条形图支持 changeData

--- src/plots/bar/index.ts
+++ src/plots/bar/index.ts
@@ -1,6 +1,7 @@
 import { Plot } from '../../core/plot';
 import { deepAssign } from '../../utils';
 import { Adaptor } from '../../core/adaptor';
+import { getDataWhetherPecentage } from '../../utils/transform/percent';
 import { BarOptions } from './types';
 import { adaptor } from './adaptor';
 
@@ -12,6 +13,15 @@ export { BarOptions };
 export class Bar extends Plot<BarOptions> {
   /** 图表类型 */
   public readonly type: string = 'bar';
+
+  /**
+   * @override
+   */
+  public changeData(data: BarOptions['data']) {
+    this.updateOption({ data });
+    const { xField, yField, isPercent } = this.options;
+    this.chart.changeData(getDataWhetherPecentage(data, xField, yField, xField, isPercent));
+  }
 
   /**
    * 获取 条形图 默认配置

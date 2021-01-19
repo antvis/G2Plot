bcd2143152e3c72ba2ea36340d57a2fa87f6bf0d
刘珍莹 <1803275951@qq.com>
2021-01-19 09:57:38 +0800

    feat: 为 treemap 新增rectstyle 和 hierarchyConfig 属性 (#2234)
    Co-authored-by: aiyin.lzy <nadia.lzy@antfin.com>

--- src/plots/treemap/utils.ts
+++ src/plots/treemap/utils.ts
@@ -2,12 +2,12 @@ import { treemap } from '../../utils/hierarchy/treemap';
 import { TreemapOptions } from './types';
 
 export function transformData(options: TreemapOptions) {
-  const { data, colorField } = options;
+  const { data, colorField, hierarchyConfig } = options;
 
   const nodes = treemap(data, {
+    ...hierarchyConfig,
     // @ts-ignore
     type: 'hierarchy.treemap',
-    tile: 'treemapResquarify',
     field: 'value',
     as: ['x', 'y'],
   });

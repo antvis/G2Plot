d1ffbcf1d85c6937c336cf5aa1b3957a7ead92d1
kasmine <736929286@qq.com>
2021-01-23 11:54:07 +0800

    test(pie): 添加饼图 utils& default-options 单测

--- __tests__/unit/plots/pie/index-spec.ts
+++ __tests__/unit/plots/pie/index-spec.ts
@@ -1,4 +1,5 @@
 import { Pie } from '../../../../src';
+import { DEFAULT_OPTIONS } from '../../../../src/plots/pie/contants';
 import { POSITIVE_NEGATIVE_DATA } from '../../../data/common';
 import { createDiv } from '../../../utils/dom';
 
@@ -166,5 +167,9 @@ describe('pie', () => {
     expect(pie.chart.getController('tooltip').getTooltipCfg().shared).toBe(false);
 
     pie.destroy();
+  });
+
+  it('defaultOptions 保持从 constants 中获取', () => {
+    expect(Pie.getDefaultOptions()).toEqual(DEFAULT_OPTIONS);
   });
 });
--- src/plots/pie/index.ts
+++ src/plots/pie/index.ts
@@ -1,14 +1,22 @@
 import { Plot } from '../../core/plot';
-import { deepAssign } from '../../utils';
 import { Adaptor } from '../../core/adaptor';
+import { adaptor, pieAnnotation } from './adaptor';
+import { DEFAULT_OPTIONS } from './contants';
 import { PieOptions } from './types';
 import { isAllZero, processIllegalData } from './utils';
-import { adaptor, pieAnnotation } from './adaptor';
 import './interactions';
 
 export { PieOptions };
 
 export class Pie extends Plot<PieOptions> {
+  /**
+   * 获取 饼图 默认配置项
+   * @static 供外部使用
+   */
+  static getDefaultOptions(): Partial<PieOptions> {
+    return DEFAULT_OPTIONS;
+  }
+
   /** 图表类型 */
   public type: string = 'pie';
 
@@ -34,52 +42,10 @@ export class Pie extends Plot<PieOptions> {
   }
 
   /**
-   * 获取 饼图 默认配置项
+   * 获取 饼图 默认配置项, 供 base 获取
    */
   protected getDefaultOptions(): Partial<PieOptions> {
-    return deepAssign({}, super.getDefaultOptions(), {
-      legend: {
-        position: 'right',
-      },
-      tooltip: {
-        shared: false,
-        showTitle: false,
-        showMarkers: false,
-      },
-      label: {
-        layout: { type: 'limit-in-plot', cfg: { action: 'ellipsis' } },
-      },
-      /** 饼图样式, 不影响暗黑主题 */
-      pieStyle: {
-        stroke: 'white',
-        lineWidth: 1,
-      },
-      /** 饼图中心文本默认样式 */
-      statistic: {
-        title: {
-          style: { fontWeight: 300, color: '#4B535E', textAlign: 'center', fontSize: '20px', lineHeight: 1 },
-        },
-        content: {
-          style: {
-            fontWeight: 'bold',
-            color: 'rgba(44,53,66,0.85)',
-            textAlign: 'center',
-            fontSize: '32px',
-            lineHeight: 1,
-          },
-        },
-      },
-      /** 默认关闭 text-annotation 动画 */
-      theme: {
-        components: {
-          annotation: {
-            text: {
-              animate: false,
-            },
-          },
-        },
-      },
-    });
+    return Pie.getDefaultOptions();
   }
 
   /**

15b881486e80d792827eaf3add61218fa728edcc
kasmine <736929286@qq.com>
2021-01-31 10:07:38 +0800

    feat(multi-view): 多图层图表支持不同view配置不同的动画效果

--- src/plots/multi-view/types.ts
+++ src/plots/multi-view/types.ts
@@ -3,6 +3,7 @@ import { Data, Meta, Options, Region, Tooltip } from '../../types';
 import { Axis } from '../../types/axis';
 import { Legend } from '../../types/legend';
 import { Geometry } from '../../adaptor/geometries/base';
+import { Animation } from '../../types/animation';
 import { Annotation } from '../../types/annotation';
 import { Interaction } from '../../types/interaction';
 
@@ -11,6 +12,7 @@ import { Interaction } from '../../types/interaction';
  */
 export type IGeometry = Geometry & {
   adjust?: Types.AdjustOption;
+  animation?: Animation;
 };
 
 /**
@@ -56,6 +58,11 @@ export type IView = {
    * annotation 配置
    */
   readonly annotations?: Annotation[];
+
+  /**
+   * animation 配置
+   */
+  readonly animation?: Animation;
 };
 
 /** 配置类型定义 */

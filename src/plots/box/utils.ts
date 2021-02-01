11c5476f144a447c39a79775050e32e269a733c0
沈建斌 <wb-sjb709429@antfin.com>
2021-02-01 10:12:18 +0800

    fix: 完善箱线图transformData类型定义及注释

--- src/plots/box/utils.ts
+++ src/plots/box/utils.ts
@@ -1,7 +1,13 @@
 import { map } from '@antv/util';
 import { BOX_RANGE } from './constant';
+import { BoxOptions } from './types';
 
-export const transformData = (data: object[], yField: string[] | string) => {
+/**
+ * @desc 将数据转换为 box 需要的的图表数据,如果yField为数组,从data中解构出对应数组值并写入data,否则直接返回data
+ * @param data
+ * @param yField
+ */
+export const transformData = (data: BoxOptions['data'], yField: BoxOptions['yField']) => {
   let newData = data;
   // formate data when `yField` is Array
   if (Array.isArray(yField)) {

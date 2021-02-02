c38aac3a3dc4446d9e1123ffa23020fa6abd1cf7
kasmine <736929286@qq.com>
2021-02-02 16:46:56 +0800

    feat(utils): 增加 view 上utils方法

--- src/utils/view.ts
+++ src/utils/view.ts
@@ -8,3 +8,24 @@ import { View } from '@antv/g2';
 export function findViewById(chart: View, id: string): View {
   return chart.views.find((view: View) => view.id === id);
 }
+
+/**
+ * 获取同 view 同一级的所有 views
+ * @param view 当前 view
+ * @returns 同一级的 views
+ * @ignore
+ */
+export function getViews(view: View): View[] {
+  const parent = view.parent;
+  return parent ? parent.views : [];
+}
+
+/**
+ * 获取同 view 同一级的 views，不包括自身
+ * @param view 当前 view
+ * @returns 同一级的 views
+ * @ignore
+ */
+export function getSilbingViews(view: View): View[] {
+  return getViews(view).filter((sub) => sub !== view);
+}

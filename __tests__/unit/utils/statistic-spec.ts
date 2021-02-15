2b39e79deb8c71b09b0086af509cbba125e3fae5
visiky <736929286@qq.com>
2021-02-15 11:09:01 +0800

    test(statistic): 补充统计文本的单测

--- __tests__/unit/utils/statistic-spec.ts
+++ __tests__/unit/utils/statistic-spec.ts
@@ -1,4 +1,10 @@
-import { adapteStyle, setStatisticContainerStyle } from '../../../src/utils/statistic';
+import { Chart } from '@antv/g2';
+import {
+  adapteStyle,
+  setStatisticContainerStyle,
+  renderStatistic,
+  renderGaugeStatistic,
+} from '../../../src/utils/statistic';
 import { ShapeStyle } from '../../../src/types';
 import { createDiv } from '../../utils/dom';
 
@@ -55,5 +61,69 @@ describe('饼图 statistics 相关处理函数', () => {
     expect(container.style['pointerEvents']).toBe('none');
     expect(container.style.color).toBe('');
     expect(container.style.fontSize).toBe('12px');
+  });
+
+  it('render-statistic', () => {
+    const div = createDiv();
+    const chart = new Chart({
+      container: div,
+      height: 200,
+    });
+    chart.coordinate({ type: 'theta', cfg: { innerRadius: 0.5, radius: 1 } });
+    renderStatistic(chart, { statistic: { title: false, content: false }, plotType: 'pie' });
+    expect(chart.getComponents().filter((c) => c.type === 'annotation').length).toBe(0);
+
+    renderStatistic(chart, { statistic: { title: {}, content: false }, plotType: 'pie' });
+    chart.render();
+    expect(chart.getComponents().filter((c) => c.type === 'annotation').length).toBe(1);
+    chart.clear();
+
+    renderStatistic(chart, { statistic: { title: {}, content: {} }, plotType: 'pie' });
+    chart.render();
+    const annotations = chart.getComponents().filter((c) => c.type === 'annotation');
+    expect(annotations.length).toBe(2);
+    expect(annotations[0].component.get('key')).toBe('top-statistic');
+    expect(annotations[1].component.get('key')).toBe('bottom-statistic');
+
+    // @ts-ignore
+    expect(div.querySelector('.g2-html-annotation').style.width).toBe(`${chart.getCoordinate().getRadius()}px`);
+    chart.clear();
+
+    renderStatistic(chart, { statistic: { title: {}, content: {} }, plotType: 'xxx' });
+    chart.render();
+    // @ts-ignore
+    expect(div.querySelector('.g2-html-annotation').style.width).toBe(`${chart.getCoordinate().getWidth()}px`);
+    chart.clear();
+  });
+
+  it('render-gauge-statistic', async () => {
+    const div = createDiv();
+    const chart = new Chart({
+      container: div,
+      height: 200,
+    });
+    chart.coordinate({ type: 'theta', cfg: { innerRadius: 0.5, radius: 1 } });
+    chart.createView();
+    renderGaugeStatistic(chart, { statistic: { title: false, content: false } });
+    expect(chart.getComponents().filter((c) => c.type === 'annotation').length).toBe(0);
+
+    renderGaugeStatistic(chart, { statistic: { title: {}, content: false } });
+    chart.render();
+    expect(chart.getComponents().filter((c) => c.type === 'annotation').length).toBe(1);
+    chart.clear();
+
+    renderGaugeStatistic(chart, {
+      statistic: {
+        title: {
+          customHtml: () => 'xxx',
+        },
+        content: {},
+      },
+    });
+    chart.render();
+    expect(chart.getComponents().filter((c) => c.type === 'annotation').length).toBe(2);
+    // @ts-ignore
+    expect(div.querySelector('.g2-html-annotation').innerText).toBe('xxx');
+    chart.clear();
   });
 });

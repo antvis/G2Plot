61a3ed810e76cd0ca03b3a5cd1d313f54b0df3cb
刘珍莹 <1803275951@qq.com>
2021-04-07 11:25:02 +0800

    feat: 为双轴图增加 setState 功能 (#2488)
    Co-authored-by: aiyin.lzy <nadia.lzy@antfin.com>

--- __tests__/unit/utils/geometry-spec.ts
+++ __tests__/unit/utils/geometry-spec.ts
@@ -1,8 +1,13 @@
 import { Chart } from '@antv/g2';
-import { findGeometry } from '../../../src/utils';
+import { findGeometry, getAllElements, getAllElementsRecursively } from '../../../src/utils';
 import { createDiv } from '../../utils/dom';
 
 const div = createDiv();
+
+const DATA = [
+  { x: 1, y: 1 },
+  { x: 2, y: 2 },
+];
 
 describe('geometry', () => {
   it('findGeometry', () => {
@@ -10,10 +15,7 @@ describe('geometry', () => {
       container: div,
     });
 
-    chart.data([
-      { x: 1, y: 1 },
-      { x: 2, y: 2 },
-    ]);
+    chart.data(DATA);
 
     const line = chart.line().position('x*y');
     const interval = chart.interval().position('x*y');
@@ -21,6 +23,51 @@ describe('geometry', () => {
     expect(findGeometry(chart, 'line')).toBe(line);
     expect(findGeometry(chart, 'interval')).toBe(interval);
     expect(findGeometry(chart, 'point')).toBe(undefined);
+
+    chart.destroy();
+  });
+
+  it('getAllElements', () => {
+    const chart = new Chart({
+      container: div,
+    });
+
+    chart.data(DATA);
+
+    chart.line().position('x*y');
+    chart.interval().position('x*y');
+
+    chart.render();
+
+    expect(getAllElements(chart).length).toBe(3);
+
+    chart.destroy();
+  });
+
+  it('getAllElementsRecursively', () => {
+    const chart = new Chart({
+      container: div,
+    });
+
+    chart.data(DATA);
+
+    chart.line().position('x*y');
+    chart.interval().position('x*y');
+
+    chart.render();
+
+    expect(getAllElementsRecursively(chart).length).toBe(3);
+
+    const childView = chart.createView();
+
+    childView.data(DATA);
+
+    childView.line().position('x*y');
+    childView.interval().position('x*y');
+
+    chart.render();
+
+    expect(getAllElementsRecursively(chart).length).toBe(6);
 
     chart.destroy();
   });

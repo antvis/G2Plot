6afc12cc7f13c0c4c1df391db96b089c2ba2b7a6
Visiky <736929286@qq.com>
2021-05-31 13:57:34 +0800

    feat(box): 重构和增强箱线图 (#2583)
    * docs(legend): 图例文档
    
    * refactor(box): 使用 geometry adaptor 处理 box，优化 tooltip & 增加单测
    
    * docs(box): 箱线图增加设置字段别名的 demo

--- src/plots/box/adaptor.ts
+++ src/plots/box/adaptor.ts
@@ -1,7 +1,7 @@
-import { isFunction, isObject } from '@antv/util';
+import { isArray } from '@antv/util';
 import { Params } from '../../core/adaptor';
-import { interaction, animation, theme } from '../../adaptor/common';
-import { findGeometry } from '../../utils';
+import { interaction, animation, theme, tooltip } from '../../adaptor/common';
+import { point, schema } from '../../adaptor/geometries';
 import { flow, pick, deepAssign } from '../../utils';
 import { AXIS_META_CONFIG_KEYS } from '../../constant';
 import { BoxOptions } from './types';
@@ -14,18 +14,38 @@ import { transformData } from './utils';
  */
 function field(params: Params<BoxOptions>): Params<BoxOptions> {
   const { chart, options } = params;
-  const { xField, yField, groupField, color } = options;
-
-  const yFieldName = Array.isArray(yField) ? BOX_RANGE : yField;
-
-  const geometry = chart.schema().position(`${xField}*${yFieldName}`).shape('box');
-
-  // set group field as color channel
-  if (groupField) {
-    geometry.color(groupField, color).adjust('dodge');
-  }
+  const { xField, yField, groupField, color, tooltip, boxStyle } = options;
 
   chart.data(transformData(options.data, yField));
+
+  const yFieldName = isArray(yField) ? BOX_RANGE : yField;
+  const rawFields = yField ? (isArray(yField) ? yField : [yField]) : [];
+
+  let tooltipOptions = tooltip;
+  if (tooltipOptions !== false) {
+    tooltipOptions = deepAssign({}, { fields: isArray(yField) ? yField : [] }, tooltipOptions);
+  }
+
+  const { ext } = schema(
+    deepAssign({}, params, {
+      options: {
+        xField,
+        yField: yFieldName,
+        seriesField: groupField,
+        tooltip: tooltipOptions,
+        rawFields,
+        schema: {
+          shape: 'box',
+          color,
+          style: boxStyle,
+        },
+      },
+    })
+  );
+
+  if (groupField) {
+    ext.geometry.adjust('dodge');
+  }
 
   return params;
 }
@@ -38,24 +58,17 @@ function outliersPoint(params: Params<BoxOptions>): Params<BoxOptions> {
 
   const outliersView = chart.createView({ padding, id: OUTLIERS_VIEW_ID });
   outliersView.data(data);
-  outliersView.axis(false);
-  const geometry = outliersView.point().position(`${xField}*${outliersField}`).shape('circle');
 
-  /**
-   * style 的几种情况
-   * g.style({ fill: 'red' });
-   * g.style('x*y*color', (x, y, color) => ({ fill: 'red' }));
-   */
-  if (isFunction(outliersStyle)) {
-    geometry.style(`${xField}*${outliersField}`, (_x: string, _outliers: number) => {
-      return outliersStyle({
-        [xField]: _x,
-        [outliersField]: _outliers,
-      });
-    });
-  } else if (isObject(outliersStyle)) {
-    geometry.style(outliersStyle);
-  }
+  point({
+    chart: outliersView,
+    options: {
+      xField,
+      yField: outliersField,
+      point: { shape: 'circle', style: outliersStyle },
+    },
+  });
+
+  outliersView.axis(false);
 
   return params;
 }
@@ -138,54 +151,9 @@ export function legend(params: Params<BoxOptions>): Params<BoxOptions> {
 }
 
 /**
- * 样式
- * @param params
- */
-function style(params: Params<BoxOptions>): Params<BoxOptions> {
-  const { chart, options } = params;
-  const { xField, yField, boxStyle } = options;
-
-  const geometry = findGeometry(chart, 'schema');
-  const yFieldName = Array.isArray(yField) ? BOX_RANGE : yField;
-
-  /**
-   * style 的几种情况
-   * g.style({ fill: 'red' });
-   * g.style('x*y*color', (x, y, color) => ({ fill: 'red' }));
-   */
-  if (isFunction(boxStyle)) {
-    geometry.style(`${xField}*${yFieldName}`, (_x: string, _y: number) => {
-      return boxStyle({
-        [xField]: _x,
-        [yFieldName]: _y,
-      });
-    });
-  } else if (isObject(boxStyle)) {
-    geometry.style(boxStyle);
-  }
-
-  return params;
-}
-
-/**
- * tooltip 配置
- * @param params
- */
-export function tooltip(params: Params<BoxOptions>): Params<BoxOptions> {
-  const { chart, options } = params;
-  const { tooltip } = options;
-
-  if (tooltip !== undefined) {
-    chart.tooltip(tooltip);
-  }
-
-  return params;
-}
-
-/**
  * 箱型图适配器
  * @param params
  */
 export function adaptor(params: Params<BoxOptions>) {
-  return flow(field, outliersPoint, meta, axis, style, legend, tooltip, interaction, animation, theme)(params);
+  return flow(field, outliersPoint, meta, axis, legend, tooltip, interaction, animation, theme)(params);
 }

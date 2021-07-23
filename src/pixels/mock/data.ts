73c27095a86e49ac1ce7ba317f11cd81fdbf95ae
酥云 <lisuwen.lsw@antgroup.com>
2021-07-23 13:53:09 +0800

    feat(tooltip): 支持基本title和fields等配置，添加crosshair和markers

---
+++ src/pixels/mock/data.ts
@@ -0,0 +1,20 @@
+export const tooltipInfo = [
+  {
+    x: 100,
+    y: 50,
+    color: '#1979C9', // todo增加 color 配置
+    data: { date: '2003-01-24', high: 66.7, open: 190, name: 'as.fe.txt' },
+  },
+  {
+    x: 100,
+    y: 100,
+    color: '#D62A0D',
+    data: { date: '2003-01-24', high: 23.7, open: 22, name: 'au.sd.txt' },
+  },
+  {
+    x: 100,
+    y: 150,
+    color: '#FAA219',
+    data: { date: '2003-01-24', high: 12.2, open: 48, name: 'as.er.txt' },
+  },
+];

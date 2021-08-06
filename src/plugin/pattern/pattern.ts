3e9b797ab4f41a51860688c8bea5c18872197f4f
Susan <527971893@qq.com>
2021-08-06 08:37:46 +0800

    feat: 初始化pattern (#2763)
    Co-authored-by: 酥云 <lisuwen.lsw@antgroup.com>

---
+++ src/plugin/pattern/pattern.ts
@@ -0,0 +1,32 @@
+export type PatternCfg = {
+  size?: [number, number]; //[width, height]
+  padding?: number;
+  fill?: string;
+  stroke?: string;
+  strokeWidth?: number;
+  opacity?: number;
+  rotate?: number;
+  isStagger?: boolean;
+  bgColor?: string | 'inherit';
+  mode?: 'repeat' | 'no-repeat' | 'repeat-x' | 'repeat-y';
+};
+
+export abstract class Pattern<O extends PatternCfg> {
+  public options: O;
+  public pattern: CanvasPattern;
+  public patternCanvas: HTMLCanvasElement;
+
+  constructor(options?: O) {
+    this.options = this.initOptions(options);
+    this.patternCanvas = document.createElement('canvas');
+    this.init();
+  }
+
+  protected abstract init();
+
+  public getCanvas() {
+    return this.patternCanvas;
+  }
+
+  protected abstract initOptions(options: O);
+}

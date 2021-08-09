7c8b4836d0887b59ce9d2cc0da29c2253a3843fb
visiky <736929286@qq.com>
2021-08-09 16:53:43 +0800

    feat(violin): 小提琴图增加状态样式设置 & 增加文档和单测

---
+++ __tests__/unit/plots/violin/state-spec.ts
@@ -0,0 +1,75 @@
+import { Violin } from '../../../../src';
+import { BASE_VIOLIN_DATA } from '../../../data/violin';
+import { createDiv } from '../../../utils/dom';
+
+describe('violin', () => {
+  const plot = new Violin(createDiv(), {
+    width: 400,
+    height: 500,
+    data: BASE_VIOLIN_DATA,
+    xField: 'type',
+    yField: 'value',
+    state: {
+      selected: {
+        style: {
+          stroke: 'blue',
+          fill: 'red',
+        },
+      },
+    },
+    box: {
+      state: {
+        selected: {
+          style: {
+            stroke: 'green',
+            fill: 'red',
+          },
+        },
+      },
+    },
+  });
+
+  plot.render();
+
+  it('setState', () => {
+    plot.setState('selected', (data) => {
+      // @ts-ignore
+      return data.x === 'SepalLength';
+    });
+    expect(plot.getStates().length).toBe(plot.views.length);
+    // @ts-ignore
+    expect(
+      plot.chart.views[0].geometries[0].elements
+        .find((ele) => ele.getModel().data.x === 'SepalLength')
+        .shape.attr('stroke')
+    ).toBe('blue');
+    // @ts-ignore
+    expect(
+      plot.chart.views[1].geometries[0].elements
+        .find((ele) => ele.getModel().data.x === 'SepalLength')
+        .shape.attr('stroke')
+    ).toBe('green');
+    // @ts-ignore
+    expect(
+      plot.chart.views[2].geometries[0].elements
+        .find((ele) => ele.getModel().data.x === 'SepalLength')
+        .shape.attr('stroke')
+    ).toBe('green');
+    // @ts-ignore
+    expect(
+      plot.chart.views[3].geometries[0].elements
+        .find((ele) => ele.getModel().data.x === 'SepalLength')
+        .shape.attr('stroke')
+    ).toBe('green');
+
+    plot.setState('active', (data) => {
+      // @ts-ignore
+      return data.x === 'SepalLength';
+    });
+    expect(plot.getStates().length).toBe(plot.views.length * 2);
+  });
+
+  afterAll(() => {
+    plot.destroy();
+  });
+});

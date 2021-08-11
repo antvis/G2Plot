7e30484f984f7c37b7872da70bdf4a3c709c465e
Visiky <736929286@qq.com>
2021-08-11 10:47:35 +0800

    refactor(pattern): 内置 pattern 的属性设计 & 优化下创建逻辑 (#2785)
    * feat(dot-pattern): 优化 dot pattern, 修复 stagger 展示
    
    * feat(square-pattern): 优化 square pattern, 修复 stagger 展示
    
    * refactor(pattern): 优化 line pattern & 删除无用代码
    
    * refactor: 一些代码上的优化 & 删除无用的代码
    
    * refactor(pattern): 初始化 canvas 的逻辑，抽取为一个 util 函数

--- src/utils/pattern/dot.ts
+++ src/utils/pattern/dot.ts
@@ -1,8 +1,71 @@
 import { DotPatternCfg } from '../../types/pattern';
 import { deepAssign } from '../../utils';
 
-export function createDotPattern(cfg: DotPatternCfg): HTMLCanvasElement {
-  const dotOptions = deepAssign(
+/**
+ * 初始化 cavnas，设置宽高等
+ */
+export function initCanvas(canvas: HTMLCanvasElement, width: number, height: number = width) {
+  // ~~~ 后续再行测试 ~~~
+  const pixelRatio = window?.devicePixelRatio || 2;
+  const logicalWidth = width;
+  const logicalHeight = height;
+  // 画布尺寸
+  canvas.width = logicalWidth * pixelRatio;
+  canvas.height = logicalHeight * pixelRatio;
+  // 显示尺寸
+  canvas.style.width = `${logicalWidth}px`;
+  canvas.style.height = `${logicalHeight}px`;
+  // ~~~ 后续再行测试 ~~~
+}
+
+/**
+ * 绘制背景
+ *
+ * @param context
+ * @param cfg
+ * @param width
+ * @param height
+ */
+export function drawBackground(
+  context: CanvasRenderingContext2D,
+  cfg: DotPatternCfg,
+  width: number,
+  height: number = width
+) {
+  const { backgroundColor, opacity } = cfg;
+
+  context.globalAlpha = opacity;
+  context.fillStyle = backgroundColor;
+
+  context.beginPath();
+  context.fillRect(0, 0, width, height);
+  context.closePath();
+}
+
+function drawDot(context: CanvasRenderingContext2D, cfg: DotPatternCfg, x: number, y: number) {
+  const { radius, fill, lineWidth, stroke, fillOpacity } = cfg;
+
+  context.beginPath();
+  context.globalAlpha = fillOpacity;
+  context.fillStyle = fill;
+  context.strokeStyle = stroke;
+  context.lineWidth = lineWidth;
+  context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  context.fill();
+  if (lineWidth) {
+    context.stroke();
+  }
+  context.closePath();
+}
+
+/**
+ * 创建 dot pattern，返回 HTMLCanvasElement
+ *
+ * @param cfg
+ * @returns HTMLCanvasElement
+ */
+export function createDotPattern(cfg: DotPatternCfg): CanvasPattern {
+  const dotCfg = deepAssign(
     {},
     {
       radius: 4,
@@ -18,62 +81,26 @@ export function createDotPattern(cfg: DotPatternCfg): HTMLCanvasElement {
     cfg
   );
 
-  const canvas = document.createElement('canvas');
-  const { radius, padding, isStagger } = dotOptions;
-  const unitSize = isStagger ? (radius + padding) * 4 : (radius + padding) * 2;
-  const dots = isStagger
-    ? [
-        [(unitSize / 4) * 1, (unitSize / 4) * 1],
-        [(unitSize / 4) * 3, (unitSize / 4) * 1],
-        [(unitSize / 4) * 0, (unitSize / 4) * 3],
-        [(unitSize / 4) * 2, (unitSize / 4) * 3],
-        [(unitSize / 4) * 4, (unitSize / 4) * 3],
-      ]
-    : [[unitSize / 2, unitSize / 2]];
+  /** 大小 */
+  const { radius, padding, isStagger } = dotCfg;
+  const dots = [[padding / 2 + radius, padding / 2 + radius]];
 
-  // 后续再行测试
-  // const dpr = window.devicePixelRatio;
-  // const logicalWidth = size / 2
-  // const logicalHeight = size / 2
-  // canvas.width = logicalWidth * dpr;
-  // canvas.height = logicalHeight * dpr;
-  // canvas.style.width = logicalWidth + 'px';
-  // canvas.style.height = logicalHeight + 'px';
-  // ctx.scale(dpr, dpr);
-
-  // console.info('dpr', dpr)
-
-  drawBackground(dotOptions, canvas, unitSize);
-  // 绘制图案
-  for (const [x, y] of dots) {
-    drawDot(dotOptions, canvas, x, y);
+  let size = radius * 2 + padding;
+  // 如果交错, size 放大两倍 交错绘制 dot
+  if (isStagger) {
+    dots.push([size + radius, size + radius]);
+    size *= 2;
   }
 
-  return canvas;
-}
-
-function drawBackground(options: DotPatternCfg, canvas: HTMLCanvasElement, unitSize: number) {
-  const { backgroundColor, opacity } = options;
+  const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
 
-  canvas.width = unitSize;
-  canvas.height = unitSize;
-  ctx.globalAlpha = opacity;
-  ctx.globalAlpha = opacity;
-  ctx.fillStyle = backgroundColor;
-  ctx.fillRect(0, 0, unitSize, unitSize);
-}
+  initCanvas(canvas, size);
+  drawBackground(ctx, dotCfg, size);
+  // 绘制图案
+  for (const [x, y] of dots) {
+    drawDot(ctx, dotCfg, x, y);
+  }
 
-function drawDot(options: DotPatternCfg, canvas: HTMLCanvasElement, x: number, y: number) {
-  const { radius, fill, lineWidth, stroke, fillOpacity } = options;
-  const ctx = canvas.getContext('2d');
-
-  ctx.beginPath();
-  ctx.globalAlpha = fillOpacity;
-  ctx.fillStyle = fill;
-  ctx.strokeStyle = stroke;
-  ctx.lineWidth = lineWidth;
-  ctx.arc(x, y, radius, 0, 2 * Math.PI);
-  ctx.fill();
-  ctx.stroke();
+  return ctx.createPattern(canvas, cfg.mode || 'repeat');
 }

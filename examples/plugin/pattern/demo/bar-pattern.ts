10bc28d7a69381d7d7757c6c7679fc5b4a7b78ee
Susan <527971893@qq.com>
2021-08-14 12:09:06 +0800

    docs: 添加pattern的demo (#2799)
    Co-authored-by: 酥云 <lisuwen.lsw@antgroup.com>

--- examples/plugin/pattern/demo/bar-pattern.ts
+++ examples/plugin/pattern/demo/bar-pattern.ts
@@ -6,17 +6,49 @@ const data = [
   { type: '分类三', value: 18 },
   { type: '分类四', value: 15 },
   { type: '分类五', value: 10 },
-  { type: '其他', value: 5 },
 ];
-
+const PATTERN_MAP = {
+  分类一: {
+    type: 'dot',
+  },
+  分类二: {
+    type: 'square',
+    cfg: {
+      size: 5,
+      padding: 0,
+      rotation: 0,
+      isStagger: true,
+    },
+  },
+  分类三: {
+    type: 'line',
+  },
+  分类四: {
+    type: 'square',
+    cfg: {
+      size: 5,
+      padding: 1,
+      rotation: 45,
+      isStagger: false,
+    },
+  },
+  分类五: {
+    type: 'line',
+    cfg: {
+      spacing: 6,
+      lineWidth: 2,
+      rotation: 90,
+    },
+  },
+};
 const plot = new Bar('container', {
   data,
   xField: 'value',
   yField: 'type',
   // 可不设置
-  // seriesField: 'type',
-  pattern: {
-    type: 'square',
+  seriesField: 'type',
+  pattern: ({ type }) => {
+    return PATTERN_MAP[type] || { type: 'dot' };
   },
 });
 

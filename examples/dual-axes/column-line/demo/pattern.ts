363eb84eda8fd86c7381e553cd44556fa5b51d01
visiky <736929286@qq.com>
2021-08-19 20:25:20 +0800

    feat(dual-axes): 双轴图支持 pattern

---
+++ examples/dual-axes/column-line/demo/pattern.ts
@@ -0,0 +1,29 @@
+import { DualAxes } from '@antv/g2plot';
+
+const data = [
+  { time: '2019-03', value: 350, count: 800 },
+  { time: '2019-04', value: 900, count: 600 },
+  { time: '2019-05', value: 300, count: 400 },
+  { time: '2019-06', value: 450, count: 380 },
+  { time: '2019-07', value: 470, count: 220 },
+];
+
+const dualAxes = new DualAxes('container', {
+  data: [data, data],
+  xField: 'time',
+  yField: ['value', 'count'],
+  geometryOptions: [
+    {
+      geometry: 'column',
+      pattern: { type: 'line' },
+    },
+    {
+      geometry: 'line',
+      lineStyle: {
+        lineWidth: 2,
+      },
+    },
+  ],
+});
+
+dualAxes.render();

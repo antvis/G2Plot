065e5bde9024485cc0e1b691ec9f94d6caa45dff
visiky <736929286@qq.com>
2021-09-03 07:31:23 +0800

    docs(venn): 补充韦恩图 demo

---
+++ examples/more-plots/venn/demo/blend-mode.ts
@@ -0,0 +1,18 @@
+import { Venn } from '@antv/g2plot';
+
+const plot = new Venn('container', {
+  padding: 0,
+  data: [
+    { sets: ['A'], size: 12, label: 'A' },
+    { sets: ['B'], size: 12, label: 'B' },
+    { sets: ['C'], size: 12, label: 'C' },
+    { sets: ['A', 'B'], size: 2, label: 'A&B' },
+    { sets: ['A', 'C'], size: 2, label: 'A&C' },
+    { sets: ['B', 'C'], size: 2, label: 'B&C' },
+    { sets: ['A', 'B', 'C'], size: 1 },
+  ],
+  // more blendMode to see: https://gka.github.io/chroma.js/#chroma-blend
+  blendMode: 'overlay',
+  pointStyle: { fillOpacity: 0.85 },
+});
+plot.render();

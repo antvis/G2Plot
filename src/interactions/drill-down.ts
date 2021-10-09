a8758fac13fa4fdb44e93297e7fedbd46f8d7d97
Visiky <736929286@qq.com>
2021-10-09 20:25:08 +0800

    feat(sunburst): 丰富旭日图交互, 点击中心可以上卷 (#2903)

--- src/interactions/drill-down.ts
+++ src/interactions/drill-down.ts
@@ -10,12 +10,29 @@ export function isParentNode(context) {
   return isArray(data.children) && data.children.length > 0;
 }
 
-registerAction('drill-down-action', DrillDownAction);
+/**
+ * 判断是否在中心
+ */
+function inCenter(context) {
+  const coordinate = context.view.getCoordinate();
+  const { innerRadius } = coordinate;
+  if (innerRadius) {
+    const { x, y } = context.event;
+    const { x: centerX, y: centerY } = coordinate.center;
+    const r = coordinate.getRadius() * innerRadius;
+    const distance = Math.sqrt((centerX - x) ** 2 + (centerY - y) ** 2);
+    return distance < r;
+  }
+  return false;
+}
 
+registerAction('drill-down-action', DrillDownAction);
 registerInteraction('drill-down', {
   showEnable: [
     { trigger: 'element:mouseenter', action: 'cursor:pointer', isEnable: isParentNode },
     { trigger: 'element:mouseleave', action: 'cursor:default' },
+    // 中心处，肯定会触发 element:mouseleave 操作
+    { trigger: 'element:mouseleave', action: 'cursor:pointer', isEnable: inCenter },
   ],
   start: [
     {
@@ -26,6 +43,12 @@ registerInteraction('drill-down', {
     {
       trigger: 'afterchangesize',
       action: ['drill-down-action:resetPosition'],
+    },
+    {
+      // 点击中心，返回上一层
+      trigger: 'click',
+      isEnable: inCenter,
+      action: ['drill-down-action:back'],
     },
   ],
 });

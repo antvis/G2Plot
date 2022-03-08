aa130d03f44e20505e5817b43591316b47e68937
ai-qing-hai <wb-xcf804241@antgroup.com>
2022-03-08 16:49:21 +0800

    feat(bar): 条形图添加动态demo

--- examples/bar/basic/demo/dynamic.ts
+++ examples/bar/basic/demo/dynamic.ts
@@ -1,4 +1,34 @@
-import { Bar } from '@antv/g2plot';
+import { Bar, G2 } from '@antv/g2plot';
+
+G2.registerAnimation('label-update', (element, animateCfg, cfg) => {
+  const startX = element.attr('x');
+  const startY = element.attr('y');
+  // @ts-ignore
+  const finalX = cfg.toAttrs.x;
+  // @ts-ignore
+  const finalY = cfg.toAttrs.y;
+  const labelContent = element.attr('text');
+  // @ts-ignore
+  const finalContent = cfg.toAttrs.text;
+
+  const distanceX = finalX - startX;
+  const distanceY = finalY - startY;
+  const numberDiff = +finalContent - +labelContent;
+
+  element.animate((ratio) => {
+    const positionX = startX + distanceX * ratio;
+    const positionY = startY + distanceY * ratio;
+    const text = (+labelContent + numberDiff * ratio).toFixed(0);
+
+    return {
+      x: positionX,
+      y: positionY,
+      text,
+    };
+  }, animateCfg);
+
+
+});
 
 // 每次动画时间 ms
 const UPDATE_TIME = 1000;
@@ -10,14 +40,24 @@ const color = {
   type3: '#65789B',
   type4: '#F6BD16',
   type5: '#7262fd',
+  type6: '#9661BC',
+  type7: '#DECFEA',
+  type8: '#BBDEDE',
+  type9: '#F08BB4',
+  type10: '#FFE0ED',
 };
 
 let data = [
   { year: `${year}年`, value: 328, type: 'type1' },
   { year: `${year}年`, value: 252, type: 'type2' },
-  { year: `${year}年`, value: 61, type: 'type3' },
-  { year: `${year}年`, value: 45, type: 'type4' },
-  { year: `${year}年`, value: 30, type: 'type5' },
+  { year: `${year}年`, value: 161, type: 'type3' },
+  { year: `${year}年`, value: 100, type: 'type4' },
+  { year: `${year}年`, value: 90, type: 'type5' },
+  { year: `${year}年`, value: 88, type: 'type6' },
+  { year: `${year}年`, value: 66, type: 'type7' },
+  { year: `${year}年`, value: 59, type: 'type8' },
+  { year: `${year}年`, value: 0, type: 'type9' },
+  { year: `${year}年`, value: 0, type: 'type10' },
 ];
 
 const bar = new Bar('container', {
@@ -25,15 +65,34 @@ const bar = new Bar('container', {
   xField: 'value',
   yField: 'type',
   seriesField: 'type',
-  padding: [0, 40, 0, 0],
-  legend: {
-    position: 'top-left',
-  },
+  padding: [10, 40, 20, 50],
+  legend: false,
   label: {
     position: 'right',
+    animate: {
+      update: {
+        animation: 'label-update',
+        duration: UPDATE_TIME,
+        easing: 'easeLinear',
+      },
+    },
   },
-  legend: false,
+  yAxis: {
+    animateOption: {
+      update: {
+        duration: 1000,
+        easing: 'easeLinear',
+      },
+    },
+    nice: false,
+  },
   color: ({ type }) => color[type],
+  animation: {
+    update: {
+      duration: UPDATE_TIME,
+      easing: 'easeLinear',
+    },
+  },
   annotations: [
     {
       type: 'text',
@@ -44,6 +103,7 @@ const bar = new Bar('container', {
         fill: '#ccc',
         fontSize: '25',
       },
+      animate: false,
     },
   ],
 });
@@ -51,14 +111,14 @@ const bar = new Bar('container', {
 bar.render();
 
 function updateData() {
-  setTimeout(() => {
-    year++;
-    data = data.map(({ type, value }) => ({
-      type,
-      year: `${year}年`,
-      value: value + Math.floor(Math.random() * 50),
-    }));
+  year++;
+  data = data.map(({ type, value }) => ({
+    type,
+    year: `${year} 年`,
+    value: value + Math.floor(Math.random() * 50),
+  }));
 
+  setTimeout(() => {
     bar.changeData(data.sort((a, b) => b.value - a.value));
 
     if (year !== 2021) {

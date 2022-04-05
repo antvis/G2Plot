e0dc8aaf76101b0e92555e530f356600e8d2e95f
visiky <736929286@qq.com>
2022-04-05 13:50:47 +0800

    chore(website): remove data-set & add bundle-analyser

--- examples/more-plots/stock/demo/large-data.ts
+++ examples/more-plots/stock/demo/large-data.ts
@@ -1,32 +1,15 @@
-import { each } from '@antv/util';
 import { Stock } from '@antv/g2plot';
-import DataSet from '@antv/data-set';
 
-fetch('https://gw.alipayobjects.com/os/bmw-prod/b341d795-679c-4e69-a3f3-03fcaedce35e.csv')
-  .then((csv) => csv.text())
-  .then((csv) => {
-    const dv = new DataSet.View().source(csv, {
-      type: 'csv',
-    });
-
-    const data = dv.rows.map((d) => {
-      const result = {};
-      each(d, (v, k) => {
-        result[k] = Number(v);
-        if (k === 'date') {
-          result[k] = new Date(Number(v)).toISOString().slice(0, 10);
-        }
-      });
-      return result;
-    });
-
+fetch('https://gw.alipayobjects.com/os/antfincdn/9SiTLAwFYu/stock-data.json')
+  .then((data) => data.json())
+  .then((data) => {
     const plot = new Stock('container', {
       data,
       xField: 'date',
       yField: ['open', 'close', 'high', 'low'],
       slider: {
-        start: 0.85,
-        end: 0.86,
+        start: 0.755,
+        end: 0.76,
       },
     });
 
--- examples/plugin/facet/demo/tree-column.ts
+++ examples/plugin/facet/demo/tree-column.ts
@@ -1,89 +1,83 @@
 import { Facet } from '@antv/g2plot';
-import { DataView } from '@antv/data-set';
 
-fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/diamond.json')
-  .then((res) => res.json())
-  .then((data) => {
-    const plot = new Facet('container', {
-      appendPadding: [0, 18, -50, 32],
-      data,
-      type: 'tree',
-      fields: ['clarity'],
-      meta: {
-        cut: {
-          sync: true,
-          // 手动进行排序，保证颜色映射正确
-          values: ['Ideal', 'Good', 'Premium', 'Very-Good', 'Fair'],
-        },
-        mean: {
-          sync: true,
-          tickCount: 5,
-          formatter: (v) => `${Math.ceil(v)}`,
-        },
+Promise.all([
+  fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/diamond.json').then(res => res.json()),
+  fetch('https://gw.alipayobjects.com/os/antfincdn/ICk9dCBRyL/facet-data.json').then(res => res.json()),
+]).then(([sourceData, datas]) => {
+  const plot = new Facet('container', {
+    appendPadding: [0, 18, -50, 32],
+    data: sourceData,
+    type: 'tree',
+    fields: ['clarity'],
+    meta: {
+      cut: {
+        sync: true,
+        // 手动进行排序，保证颜色映射正确
+        values: ['Ideal', 'Good', 'Premium', 'Very-Good', 'Fair'],
       },
-      // tree-facet 连接线样式和是否平滑
-      line: {
-        style: {
-          stroke: '#dedede',
-        },
-        smooth: true,
+      mean: {
+        sync: true,
+        tickCount: 5,
+        formatter: (v) => `${Math.ceil(v)}`,
       },
-      axes: {},
-      tooltip: { showMarkers: false },
-      eachView: (view, facet) => {
-        const { rowIndex } = facet || {};
-        const dv = new DataView();
-        dv.source(facet.data).transform({
-          type: 'aggregate',
-          fields: ['price'],
-          operations: ['mean'],
-          as: ['mean'],
-          groupBy: ['cut'],
-        });
-        if (rowIndex === 0) {
-          return {
-            type: 'pie',
-            options: {
-              data: dv.rows,
-              meta: {
-                mean: {
-                  // 关闭饼图 'mean'字段的 scale 同步
-                  sync: false,
-                },
-              },
-              angleField: 'mean',
-              colorField: 'cut',
-              radius: 1,
-              pieStyle: { stroke: null },
-              // 添加动画
-              animation: {},
-              // 添加交互
-              interactions: [{ type: 'element-active' }],
-            },
-          };
-        }
+    },
+    // tree-facet 连接线样式和是否平滑
+    line: {
+      style: {
+        stroke: '#dedede',
+      },
+      smooth: true,
+    },
+    axes: {},
+    tooltip: { showMarkers: false },
+    eachView: (view, facet) => {
+      const { rowIndex } = facet || {};
+      const facetData = datas[rowIndex];
+      if (rowIndex === 0) {
         return {
-          type: 'column',
+          type: 'pie',
           options: {
-            data: dv.rows,
+            data: facetData,
             meta: {
               mean: {
                 // 关闭饼图 'mean'字段的 scale 同步
-                sync: rowIndex === 0 ? false : true,
+                sync: false,
               },
             },
-            xField: 'cut',
-            yField: 'mean',
-            seriesField: 'cut',
-            columnStyle: { fillOpacity: 0.85 },
+            angleField: 'mean',
+            colorField: 'cut',
+            radius: 1,
+            pieStyle: { stroke: null },
             // 添加动画
             animation: {},
             // 添加交互
-            interactions: [{ type: 'element-active' }, { type: 'active-region' }],
+            interactions: [{ type: 'element-active' }],
           },
         };
-      },
-    });
-
-    plot.render();
+      }
+      return {
+        type: 'column',
+        options: {
+          data: facetData,
+          meta: {
+            mean: {
+              // 关闭饼图 'mean'字段的 scale 同步
+              sync: rowIndex === 0 ? false : true,
+            },
+          },
+          xField: 'cut',
+          yField: 'mean',
+          seriesField: 'cut',
+          columnStyle: { fillOpacity: 0.85 },
+          // 添加动画
+          animation: {},
+          // 添加交互
+          interactions: [{ type: 'element-active' }, { type: 'active-region' }],
+        },
+      };
+    },
   });
+
+  plot.render();
+});
+

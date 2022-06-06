3d076deef20e899e79091a355ee83650785e912b
ai-qing-hai <65594180+ai-qing-hai@users.noreply.github.com>
2022-06-06 17:08:07 +0800

    fix(bullet): 子弹图 添加单数值range/measures 、 多 target 数据修改兼容 (#3231)
    * fix(Bullet): 修复子弹图 多 target 时 的数据兼容问题
    
    * fix(Bullet): 修复子弹图 多 target 时 的数据兼容问题 -2
    
    * fix(bullet): 子弹图 添加单数值range/measures 、 多 target 数据修改兼容
    
    * fix(bullet): 子弹图 添加单数值range/measures 、 多 target 数据修改兼容 -2
    
    * fix(bullet): 子弹图 处理数据函数 分离分组字段数据 key 值处理函数
    
    * fix(bullet): 去除target 排序，让用户自己去排颜色
    
    Co-authored-by: ai-qing-hai <wb-xcf804241@antgroup.com>

--- __tests__/unit/plots/bullet/data-spec.ts
+++ __tests__/unit/plots/bullet/data-spec.ts
@@ -48,6 +48,90 @@ describe('bullet*data*transfrom', () => {
     expect(ds).toEqual(transDS);
   });
 
+  it('data*moretarget', () => {
+    const bulletData = [{ title: '数学', ranges: [30, 50, 100], measures: [20], target: [90, 85, 66] }];
+    // 校验数据转换
+    const { ds } = transformData({
+      data: bulletData,
+      measureField: 'measures',
+      rangeField: 'ranges',
+      targetField: 'target',
+      xField: 'title',
+    });
+
+    // 转化为的数据应该为
+    const transDS = [
+      {
+        title: '数学',
+        ranges: 30,
+        rKey: 'ranges_0',
+      },
+      {
+        title: '数学',
+        ranges: 20,
+        rKey: 'ranges_1',
+      },
+      {
+        title: '数学',
+        ranges: 50,
+        rKey: 'ranges_2',
+      },
+      {
+        title: '数学',
+        measures: 20,
+        mKey: 'measures', // 只有一个数据
+      },
+      {
+        title: '数学',
+        target: 66,
+        tKey: 'target_0',
+      },
+      {
+        title: '数学',
+        target: 85,
+        tKey: 'target_1',
+      },
+      {
+        title: '数学',
+        target: 90,
+        tKey: 'target_2',
+      },
+    ];
+    expect(ds).toEqual(transDS);
+  });
+
+  it('data*one', () => {
+    const bulletData = [{ title: '数学', ranges: 100, measures: 20, target: 90 }];
+    // 校验数据转换
+    const { ds } = transformData({
+      data: bulletData,
+      measureField: 'measures',
+      rangeField: 'ranges',
+      targetField: 'target',
+      xField: 'title',
+    });
+
+    // 转化为的数据应该为
+    const transDS = [
+      {
+        title: '数学',
+        ranges: 100,
+        rKey: 'ranges_0',
+      },
+      {
+        title: '数学',
+        measures: 20,
+        mKey: 'measures', // 只有一个数据
+      },
+      {
+        title: '数学',
+        target: 90,
+        tKey: 'target',
+      },
+    ];
+    expect(ds).toEqual(transDS);
+  });
+
   it('data*transfrom*modify', () => {
     const bulletData = [{ title: 'antv', subTitle: 'g2', count: [30, 50, 100, 150], measures: [20, 30], target: 85 }];
     // 校验数据转换
--- src/plots/bullet/utils.ts
+++ src/plots/bullet/utils.ts
@@ -5,6 +5,18 @@ type TransformData = {
   max: number;
   ds: any[];
 };
+
+/**
+ * 获取分类字段 key 值 一个分类值的时候， 返回非索引 key 值，在 tooltip 不做索引区分
+ * @param values 数据量
+ * @param field 指标字段
+ * @param index 索引
+ * @returns string
+ */
+function getSeriesFieldKey(values: number[], field: string, index: number): string {
+  return values.length > 1 ? `${field}_${index}` : `${field}`;
+}
+
 /**
  * bullet 处理数据
  * @param options
@@ -15,29 +27,37 @@ export function transformData(options: BulletOptions): TransformData {
   const scales: number[] = [];
   data.forEach((item: any, index: number) => {
     // 构建 title * range
-    item[rangeField].sort((a: number, b: number) => a - b);
-    item[rangeField].forEach((d: number, i: number) => {
-      const range = i === 0 ? d : item[rangeField][i] - item[rangeField][i - 1];
+    const rangeValues = [item[rangeField]].flat();
+    rangeValues.sort((a: number, b: number) => a - b);
+    rangeValues.forEach((d: number, i: number) => {
+      const range = i === 0 ? d : rangeValues[i] - rangeValues[i - 1];
       ds.push({
         rKey: `${rangeField}_${i}`,
         [xField]: xField ? item[xField] : String(index), // 没有xField就用索引
         [rangeField]: range,
       });
     });
+
     // 构建 title * measure
-    item[measureField].forEach((d: number, i: number) => {
+    const measureValues = [item[measureField]].flat();
+    measureValues.forEach((d: number, i: number) => {
       ds.push({
-        mKey: item[measureField].length > 1 ? `${measureField}_${i}` : `${measureField}`, // 一个数据就不带索引了
+        mKey: getSeriesFieldKey(measureValues, measureField, i),
         [xField]: xField ? item[xField] : String(index),
         [measureField]: d,
       });
     });
+
     // 构建 title * target
-    ds.push({
-      tKey: `${targetField}`,
-      [xField]: xField ? item[xField] : String(index),
-      [targetField]: item[targetField],
+    const targetValues = [item[targetField]].flat();
+    targetValues.forEach((d: number, i: number) => {
+      ds.push({
+        tKey: getSeriesFieldKey(targetValues, targetField, i),
+        [xField]: xField ? item[xField] : String(index),
+        [targetField]: d,
+      });
     });
+
     // 为了取最大值和最小值，先存储
     scales.push(item[rangeField], item[measureField], item[targetField]);
   });

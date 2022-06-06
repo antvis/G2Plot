feca4311aedf8e1b130391fcf2bb5142da421013
ai-qing-hai <wb-xcf804241@antgroup.com>
2022-06-06 15:08:01 +0800

    fix(bullet): 子弹图 添加单数值range/measures 、 多 target 数据修改兼容 -2

--- src/plots/bullet/utils.ts
+++ src/plots/bullet/utils.ts
@@ -15,58 +15,37 @@ export function transformData(options: BulletOptions): TransformData {
   const scales: number[] = [];
   data.forEach((item: any, index: number) => {
     // 构建 title * range
-    if (Array.isArray(item[rangeField])) {
-      item[rangeField].sort((a: number, b: number) => a - b);
-      item[rangeField].forEach((d: number, i: number) => {
-        const range = i === 0 ? d : item[rangeField][i] - item[rangeField][i - 1];
-        ds.push({
-          rKey: `${rangeField}_${i}`,
-          [xField]: xField ? item[xField] : String(index), // 没有xField就用索引
-          [rangeField]: range,
-        });
-      });
-    } else {
+    const rangeFields = [item[rangeField]].flat();
+    rangeFields.sort((a: number, b: number) => a - b);
+    rangeFields.forEach((d: number, i: number) => {
+      const range = i === 0 ? d : rangeFields[i] - rangeFields[i - 1];
       ds.push({
-        rKey: `${rangeField}`,
-        [xField]: xField ? item[xField] : String(index),
-        [rangeField]: item[rangeField],
+        rKey: rangeFields.length > 1 ? `${rangeField}_${i}` : `${rangeField}`, // 一个数据就不带索引了
+        [xField]: xField ? item[xField] : String(index), // 没有xField就用索引
+        [rangeField]: range,
       });
-    }
+    });
 
     // 构建 title * measure
-    if (Array.isArray(item[measureField])) {
-      item[measureField].forEach((d: number, i: number) => {
-        ds.push({
-          mKey: item[measureField].length > 1 ? `${measureField}_${i}` : `${measureField}`, // 一个数据就不带索引了
-          [xField]: xField ? item[xField] : String(index),
-          [measureField]: d,
-        });
-      });
-    } else {
+    const measureFields = [item[measureField]].flat();
+    measureFields.forEach((d: number, i: number) => {
       ds.push({
-        mKey: `${measureField}`, // 一个数据就不带索引了
+        mKey: measureFields.length > 1 ? `${measureField}_${i}` : `${measureField}`, // 一个数据就不带索引了
         [xField]: xField ? item[xField] : String(index),
-        [measureField]: item[measureField],
+        [measureField]: d,
       });
-    }
+    });
 
     // 构建 title * target
-    if (Array.isArray(item[targetField])) {
-      item[targetField].sort((a: number, b: number) => a - b);
-      item[targetField].forEach((d: number, i: number) => {
-        ds.push({
-          tKey: item[targetField].length > 1 ? `${targetField}_${i}` : `${targetField}`, // 一个数据就不带索引了
-          [xField]: xField ? item[xField] : String(index),
-          [targetField]: d,
-        });
-      });
-    } else {
+    const targetFields = [item[targetField]].flat();
+    targetFields.sort((a: number, b: number) => a - b);
+    targetFields.forEach((d: number, i: number) => {
       ds.push({
-        tKey: `${targetField}`,
+        tKey: targetFields.length > 1 ? `${targetField}_${i}` : `${targetField}`, // 一个数据就不带索引了
         [xField]: xField ? item[xField] : String(index),
-        [targetField]: item[targetField],
+        [targetField]: d,
       });
-    }
+    });
 
     // 为了取最大值和最小值，先存储
     scales.push(item[rangeField], item[measureField], item[targetField]);

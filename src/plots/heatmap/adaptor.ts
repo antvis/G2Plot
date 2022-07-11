d05ce063b7a5fc4b4a39ab3927bfc45d3cca38b7
ai-qing-hai <wb-xcf804241@antgroup.com>
2022-07-11 16:57:38 +0800

    fix(heatmap): 解决部分ci

--- src/plots/heatmap/adaptor.ts
+++ src/plots/heatmap/adaptor.ts
@@ -9,7 +9,8 @@ import { HeatmapOptions } from './types';
 
 function geometry(params: Params<HeatmapOptions>): Params<HeatmapOptions> {
   const { chart, options } = params;
-  const { data, type, xField, yField, colorField, sizeField, sizeRatio, shape, color, tooltip, heatmapStyle, meta } = options;
+  const { data, type, xField, yField, colorField, sizeField, sizeRatio, shape, color, tooltip, heatmapStyle, meta } =
+    options;
 
   chart.data(data);
   let geometryType = 'polygon';
@@ -50,12 +51,12 @@ function geometry(params: Params<HeatmapOptions>): Params<HeatmapOptions> {
             shape &&
             (sizeField
               ? (dautm) => {
-                  const field = data.map((row) => row[sizeField]);
-                  let { min, max } = meta?.[sizeField] || {};
-                  min = min || Math.min(...field);
-                  max = max || Math.max(...field);
-                  return [shape, (get(dautm, sizeField) - min) / (max - min), checkedSizeRatio];
-                }
+                const field = data.map((row) => row[sizeField]);
+                let { min, max } = meta?.[sizeField] || {};
+                min = min || Math.min(...field);
+                max = max || Math.max(...field);
+                return [shape, (get(dautm, sizeField) - min) / (max - min), checkedSizeRatio];
+              }
               : () => [shape, 1, checkedSizeRatio]),
           color: color || (colorField && chart.getTheme().sequenceColors.join('-')),
           style: heatmapStyle,

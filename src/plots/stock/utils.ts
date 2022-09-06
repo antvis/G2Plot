56da06f2edae1eae794cc89d7f0a23bb92f3c54d
Victor Korzunin <5180700+floydspace@users.noreply.github.com>
2022-09-06 23:04:24 +0200

    fix(stock): avoid mutating stock data item
    issue #3301

--- src/plots/stock/utils.ts
+++ src/plots/stock/utils.ts
@@ -7,8 +7,9 @@ import { TREND_FIELD, TREND_DOWN, TREND_UP, Y_FIELD } from './constant';
  * @param yField
  */
 export function getStockData(data, yField) {
-  return map(data, (obj) => {
-    if (isArray(yField)) {
+  return map(data, (item) => {
+    const obj = item && { ...item };
+    if (isArray(yField) && obj) {
       const [open, close, high, low] = yField;
       obj[TREND_FIELD] = obj[open] <= obj[close] ? TREND_UP : TREND_DOWN;
       obj[Y_FIELD] = [obj[open], obj[close], obj[high], obj[low]];

bb34d076fad96ec87f6e04bc194bc3999f9d0b3a
pepper-nice <109653633+pepper-nice@users.noreply.github.com>
2023-03-14 21:11:41 -0700

    feat: add column plot (#3496)

---
+++ __tests__/integration/plots/column-basic.ts
@@ -0,0 +1,19 @@
+import { Column } from '../../../src';
+
+export function columnBasic(container, renderer = 'canvas') {
+  return new Column(container, {
+    renderer,
+    data: [
+      { genre: 'Sports', sold: 275 },
+      { genre: 'Strategy', sold: 115 },
+      { genre: 'Action', sold: 120 },
+      { genre: 'Shooter', sold: 350 },
+      { genre: 'Other', sold: 150 },
+    ],
+    encode: {
+      x: 'genre',
+      y: 'sold',
+      color: 'genre',
+    },
+  });
+}
